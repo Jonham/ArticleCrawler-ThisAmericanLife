d2d02a67ce40ac5cfb265ef3fd4cad49f82d55d3
Jonham.Chen <me@jonham.cn>
2022-02-07 10:00:56 +0800

    feat: add 760 761

---
+++ article/760.article.ts
@@ -0,0 +1,82 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 760,
+  "title": "A City Walks Into an Investigation",
+  "brief": "Last week's story continues, about a Michigan couple who walked into a police officer's house and made a disturbing discovery. This week: the police officer suffers the consequences and so does the couple.",
+  "updateTime": "January 28, 2022",
+  "coverURL": "https://hw1.thisamericanlife.org/sites/default/files/styles/portrait/public/episodes/images/tal-760-acitywalksinto-joanwong.jpg?itok=znwPCitF",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/b8d6a83e-e68c-40bb-8b72-f190bfc38352/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=b8d6a83e-e68c-40bb-8b72-f190bfc38352",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira summarizes part one of our story.",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Chief Lewis Adds It Up",
+      "brief": "The police chief in Muskegon conducts his own personal investigation into Officer Anderson’s interactions with Black people on the job. He doesn’t like what he discovers.",
+      "author": "Ben Calhoun",
+      "timestamp": 154,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "The Way It Actually Happened",
+      "brief": "Officer Anderson is fired based on the chief’s findings. But that’s not what’s said publicly.",
+      "author": "",
+      "timestamp": 550,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "How To Fire a Police Officer",
+      "brief": "Muskegon’s chief of police explains why he decided he couldn’t mention any of the stuff he found troubling about Officer Anderson’s treatment of Black people on the job.",
+      "author": "",
+      "timestamp": 752,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "People Who Got the Short End of Anderson’s Deal",
+      "brief": "Duke Stalling, whose mother’s birthday party was pepper sprayed by Officer Anderson when he showed up on a noise complaint, reacts to the city’s deal with Anderson.",
+      "author": "",
+      "timestamp": 1253,
+      "song": []
+    },
+    {
+      "index": "Act Five",
+      "title": "No Good Deed Goes Unpunished",
+      "brief": "Turns out, the couple who originally brought Officer Anderson into the spotlight with their Facebook post had encountered Anderson before in Muskegon. Specifically, at a traffic stop where things got ugly.",
+      "author": "",
+      "timestamp": 1618,
+      "song": []
+    },
+    {
+      "index": "Act Six",
+      "title": "Was It Worth It?",
+      "brief": "The Mathises consider if going public with what they found in Officer Anderson’s house was worth it, given what their personal lives have been like since.",
+      "author": "",
+      "timestamp": 2394,
+      "song": []
+    },
+    {
+      "index": "Coda",
+      "title": "Coda",
+      "brief": "Ira and Ben consider how officers could be flagged for bad policing in a more systematic way, rather than the freak occurrence of a couple walking into an officer’s home and finding a KKK application up on his wall.",
+      "author": "",
+      "timestamp": 2676,
+      "song": [
+        {
+          "raw": "“What Am I” by Jordan Mackampa",
+          "singer": "Jordan Mackampa",
+          "name": "What Am I"
+        }
+      ]
+    }
+  ]
+}
---
+++ article/761.article.ts
@@ -0,0 +1,42 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 761,
+  "title": "The Trojan Horse Affair",
+  "brief": "A while back, one of our producers Brian Reed was in England giving a speech about the podcast he'd hosted, S-Town. A journalism student approached him, asking for advice about a story he wanted to look into – something that’d been big news in Britain, something he’d thought about for years. Brian and the student, Hamza Syed, decided to team up to try and solve the mystery at the heart of that story. The original idea was to put this on our show, but it got too big. Too many twists and turns! This week, it rolls out as a spectacularly great podcast called The Trojan Horse Affair. We’re excited to bring you their first episode today.",
+  "updateTime": "February 4, 2022",
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/uncropped/public/episodes/images/tal-trojanhorseaffair-hokyoungkim.jpg?itok=R4EurTQm",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/a1f1d9b8-b3b4-4803-875b-dd71d6985e75/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=a1f1d9b8-b3b4-4803-875b-dd71d6985e75",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira introduces the first part of the latest podcast from Brian Reed (<em>S-Town</em>)&nbsp;and Hamza Syed — which all started when Hamza was a student with a burning question.",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Act One",
+      "brief": "Hamza and Brian visit Tahir Alam, the alleged mastermind of a plot to ‘Islamize’ Birmingham’s schools.",
+      "author": "Brian Reed, Hamza Syed",
+      "timestamp": 92,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Act Two",
+      "brief": "As Tahir’s revamp of Park View School continues to grow and deliver stunning results, his reputation grows — as does resentment at his blunt, unvarnished style. And then came the letter, and hoax or no, the consequences of its contents began to unravel Tahir’s life’s work almost immediately.",
+      "author": "",
+      "timestamp": 1886,
+      "song": [
+        {
+          "raw": "“Froggy” by Jaykae and Dapz on the Map",
+          "singer": "Jaykae and Dapz on the Map",
+          "name": "Froggy"
+        }
+      ]
+    }
+  ]
+}
