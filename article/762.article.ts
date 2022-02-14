d03aa2524e960abe7bb896cf793970303fa563db
Jonham <me@jonham.cn>
2022-02-14 22:17:06 +0800

    feat: add ts and fetch 762

---
+++ article/762.article.ts
@@ -0,0 +1,42 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 762,
+  "title": "Apocalypse Creep",
+  "brief": "A woman wakes up and discovers her backyard has disappeared, and other stories from places slowly coming apart.",
+  "updateTime": "February 11, 2022",
+  "coverURL": "https://hw2.thisamericanlife.org/sites/default/files/styles/portrait/public/episodes/images/tal-apocalypsecreep-izharcohen.jpg?itok=kD75tHqi",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/71dd7fd6-1256-4ff7-839f-0adfd9a6e6f6/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=71dd7fd6-1256-4ff7-839f-0adfd9a6e6f6",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira talks with a radio announcer in Truckee, California, about the increasingly alarming content of his fishing reports.",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Apocalypse Now-ish",
+      "brief": "Producer Alix Spiegel reports on the city of Pacifica, California. A huge fight erupted there after it was asked to start planning for sea level rise.",
+      "author": "Alix Spiegel",
+      "timestamp": 729,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Resilience is Futile",
+      "brief": "Miami writer Mario&nbsp;Alejandro Ariza on how his city is trying to beat back the impacts of climate change.",
+      "author": "Mario Alejandro Ariza",
+      "timestamp": 3085,
+      "song": [
+        {
+          "raw": "“Stormy Weather” by The Pixies",
+          "singer": "The Pixies",
+          "name": "Stormy Weather"
+        }
+      ]
+    }
+  ]
+}
