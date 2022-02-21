7b13777d120a663e801b5e25d7291ff31dcf9730
jonham <jonham@qq.com>
2022-02-21 22:22:42 +0800

    feat: add article 466

---
+++ article/466.article.ts
@@ -0,0 +1,52 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 466,
+  "title": "Blackjack",
+  "brief": "The casino game everyone thinks they can beat.",
+  "preUpdateTime": "June 8, 2012",
+  updateTime: 'Feb 18, 2022',
+  isUpdate: true,
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/466.jpg?itok=ZlsrgpmM",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/b23011fa-15d2-4bd0-82b5-9f6c56b4d25e/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=b23011fa-15d2-4bd0-82b5-9f6c56b4d25e",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Host Ira Glass and producer Robyn Semien get a blackjack lesson from Andy Bloch, who played for the MIT blackjack team. He teaches them the basics of card-counting, the technique that gives players an advantage against the house — enough of an advantage that most casinos will ask you to leave if they catch you doing it.",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Render Unto Caesar's Palace What Is Due To Caesar's Palace",
+      "brief": "Jack Hitt tells the story of the Christian card counting team featured in the documentary <em>Holy Rollers</em>, and why they see no contradiction in being devout Christians who spend their days in casinos. Jack is the author of the book <em>Bunch of Amateurs</em>.",
+      "author": "Jack Hitt",
+      "timestamp": 536,
+      "song": []
+    },
+    {
+      "index": "Act 2",
+      "title": "Part Two",
+      "brief": "Ira and Robyn go to the casino to try out their newfound card counting skills.",
+      "author": "Ira Glass",
+      "timestamp": 1598,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Harrah's Today, Gone Tomorrow",
+      "brief": "Producer Sarah Koenig tells the story of a woman who sued the casino where she lost her inheritance, saying that it was to blame, not her. The story was inspired by a chapter in <em>The Power of Habit</em>, by Charles Duhigg.",
+      "author": "Sarah Koenig",
+      "timestamp": 1900,
+      "song": [
+        {
+          "raw": "“All I Wanna Do is Play Cards ” by Corb Lund",
+          "singer": "Corb Lund",
+          "name": "All I Wanna Do is Play Cards "
+        }
+      ]
+    }
+  ]
+}
