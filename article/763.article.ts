2291fca8748319e59ac52340210c3d6a0206846b
Jonham.Chen <me@jonham.cn>
2022-03-01 10:13:07 +0800

    feat: fetch 763

---
+++ article/763.article.ts
@@ -0,0 +1,52 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 763,
+  "title": "The Other Mr. President",
+  "brief": "Stories about Vladimir Putin. Did he come to power in 1999 by killing hundreds of innocent Russians? How’s he really seen in his home country? This show is a mix of old and new stories we’ve done about him.",
+  "updateTime": "February 25, 2022",
+  "coverURL": "https://hw4.thisamericanlife.org/sites/default/files/styles/portrait/public/episodes/images/putin-maximfomenko.jpg?itok=_o6_UOTN",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/70c3aa87-4933-4637-8486-a0e8198d1e20/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=70c3aa87-4933-4637-8486-a0e8198d1e20",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Late at night on the evening Russia invaded Ukraine, Ira talks to two people who escaped to Lviv, near the Polish border: a woman we call Natalie, and the Ukraine Correspondent for The Economist, Richard Ensor. Natalie’s harrowing story about escaping Kyiv is <em>not</em> the sort of war story that makes you think, \"I can't imagine what it'd be like to go through that.” In fact it’s just the opposite.",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Going in with a Bang",
+      "brief": "Back in 1999 there was series of bombings of apartment buildings in Moscow and across Russia. 300 people died. It happened just as Vladimir Putin was coming to power.",
+      "author": "Robyn Semien",
+      "timestamp": 775,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Mr. Popular",
+      "brief": "Vladmir Putin’s approval rating among Russians is always stunningly high. Ira talks to reporter Charles Maynes to find out if that number is real and how it could be that high.",
+      "author": "Ira Glass",
+      "timestamp": 2068,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Maybe Pay Attention To The Man Behind The Curtain  ",
+      "brief": "Disinformation and propaganda works differently in Putin’s Russia than it did during the Soviet Union. Instead of tamping down the opposition, the Russian government works to control the opposition.",
+      "author": "Ira Glass",
+      "timestamp": 2740,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "A Matter of Principal (podcast only)",
+      "brief": "Protestors came out across Russia after the Ukraine invasion. In this act, that we first broadcast in 2017, we hear from young people who attended anti-government protests that swept through Russia.",
+      "author": "Ira Glass",
+      "timestamp": 3678,
+      "song": []
+    }
+  ]
+}
