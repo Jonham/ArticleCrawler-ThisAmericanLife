7df250de88894791f58be2bbd98616c849a97e94
Jonham.Chen <me@jonham.cn>
2022-03-17 09:56:30 +0800

    feat: add 764 and add `./run.ps1` shortcut for collecting work

---
+++ article/764.article.ts
@@ -0,0 +1,42 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 764,
+  "title": "School's Out Forever",
+  "brief": "The pandemic broke school. Can we ever go back?",
+  "updateTime": "March 11, 2022",
+  "coverURL": "https://hw4.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal_schoolsout_danielfishel.jpg?itok=IIJ_Bumw",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/f82a132a-5656-4824-9933-39f475ac5f5a/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=f82a132a-5656-4824-9933-39f475ac5f5a",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Host Chana Joffe-Walt talks to teachers and principals about the unique challenges the pandemic has created in their daily working lives.&nbsp;(7&nbsp; minutes)",
+      "author": "Chana Joffe-Walt",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Big Mother Is Watching",
+      "brief": "When school closed and threw routines into disarray, one mom and daughter figured out a new way to make it work.",
+      "author": "Chana Joffe-Walt",
+      "timestamp": 529,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "The Case of The Vanishing Sixth Grader",
+      "brief": "The pandemic moved lots of families around, and many children simply vanished from school, in person, and online.",
+      "author": "Chana Joffe-Walt",
+      "timestamp": 2105,
+      "song": [
+        {
+          "raw": "“School Is Out” by Gary U.S. Bonds",
+          "singer": "Gary U.S. Bonds",
+          "name": "School Is Out"
+        }
+      ]
+    }
+  ]
+}
