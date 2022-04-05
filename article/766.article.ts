600facda8eb9ecca568002dc5b75ec2d32cb41e3
Jonham.Chen <me@jonham.cn>
2022-04-06 07:48:54 +0800

    feat: update 766

---
+++ article/766.article.ts
@@ -0,0 +1,58 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 766,
+  "title": "Well Someone Had to Do SOMETHING!",
+  "brief": "People trying to jump in and solve other people's problems, putting themselves directly in the gap between the problem and the solution.",
+  "updateTime": "April 1, 2022",
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-766-ukraine-heidilevine.jpg?itok=fuZreEQU",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/38e10e78-f1a9-4c2b-9215-61aebe46acf3/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=38e10e78-f1a9-4c2b-9215-61aebe46acf3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Host Sean Cole tells us the story of Seattle residents who’ve had their bikes stolen, only to get them back in a way they really are not expecting.",
+      "author": "Sean Cole",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Cold Call to Action",
+      "brief": "Producer Alix Spiegel tells the story of a Lithuanian man who devises a plan to solve a problem related to the war in Ukraine.",
+      "author": "Alix Spiegel",
+      "timestamp": 719,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Zoo Unto Others",
+      "brief": "Sean Cole speaks to Washington Post reporter Siobhan O’Grady about her visit to the zoo in Kyiv.",
+      "author": "Sean Cole",
+      "timestamp": 1652,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "One Last Thing After I Go",
+      "brief": "Producer Aviva DeKornfeld tells the story of Bill Edgar, who accidentally ended up helping people have a say at a moment when most people don’t get to say anything at all.",
+      "author": "Aviva DeKornfeld",
+      "timestamp": 2127,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "A Funny Thing Happened When We Were Already AT the Theater",
+      "brief": "Producer Chana Joffe-Walt tells the story of a woman who took it upon herself to do something in an effort to help, and was not well received.&nbsp;(8 minutes)If you want to find more from Tiffany King, check out her Instagram for links to her comedy and other work.&nbsp;",
+      "author": "Chana Joffe-Walt",
+      "timestamp": 2873,
+      "song": [
+        {
+          "raw": "“If You Got a Problem” by Joy Oladokun",
+          "singer": "Joy Oladokun",
+          "name": "If You Got a Problem"
+        }
+      ]
+    }
+  ]
+}
