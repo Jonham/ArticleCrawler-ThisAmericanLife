6e3a1a4d1360ab1255f82112e540a94f60b2c580
Jonham.Chen <me@jonham.cn>
2022-04-11 22:38:17 +0800

    feat: add 767

---
+++ article/767.article.ts
@@ -0,0 +1,42 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 767,
+  "title": "Do Not Go Gentle",
+  "brief": "In this moment when autocrats and almost-autocrats are getting bolder and more powerful, we bring you two stories of resistance, from Hungary and Russia.",
+  "updateTime": "April 8, 2022",
+  "coverURL": "https://hw1.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal_767-hungary-hokyoungkim.jpg?itok=CHtgZNaT",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/ff916a0e-6818-49ba-8b45-bd43bad9d10c/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=ff916a0e-6818-49ba-8b45-bd43bad9d10c",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Host Ira Glass zooms in on five surreal minutes of Hungary’s opposition campaign.&nbsp;(5 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "The Brothers Harangazo",
+      "brief": "Producer Zoe Chace spends time with two brothers in Hungary, trying to win an election that’s basically impossible for them to win.",
+      "author": "Zoe Chace",
+      "timestamp": 315,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Putin’s On Our Side",
+      "brief": "One of the last independent newspapers in Russia finds new ways to cover a war that the government doesn’t want them to cover.",
+      "author": "Robyn Semien",
+      "timestamp": 2018,
+      "song": [
+        {
+          "raw": "“Rage” by The Jerks",
+          "singer": "The Jerks",
+          "name": "Rage"
+        }
+      ]
+    }
+  ]
+}
