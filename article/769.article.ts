bf9c7a7157a6a6f7db48e55a8025f1c9df0a1e89
Jonham <me@jonham.cn>
2022-05-03 14:08:38 +0800

    update 769

---
+++ article/769.article.ts
@@ -0,0 +1,53 @@
+import { ArticleProto } from "../parseContent";
+
+export const info: ArticleProto = {
+  number: 769,
+  title: "The Reluctant Explorer",
+  brief:
+    "A man finds himself thrust into a new world he didn’t necessarily ask to visit. He takes a look around.\nView episode details",
+  updateTime: "April 29, 2022",
+  coverURL:
+    "https://hw3.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-769-reluctantexplorer-edwardmonaghan_0.jpg?itok=Azb2zvOe",
+  audioURL:
+    "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/9ba1a271-26e8-42ea-a470-b902faca42ad/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=9ba1a271-26e8-42ea-a470-b902faca42ad",
+  acts: [
+    {
+      index: "Prologue",
+      title: "Prologue",
+      brief:
+        "Ken had created a website and abandoned it. Years later, a stranger reaches out to him and tells him he can make money off it.",
+      author: "Ira Glass",
+      timestamp: 0,
+      song: [],
+    },
+    {
+      index: "Act One",
+      title: "Planet of the Apes",
+      brief:
+        "Ira Glass follows Ken as he tries to figure out whether or not the man who reached out to him actually scammed him. Ken learns about a mysterious bot that may have been involved and worries his life may be in danger.",
+      author: "",
+      timestamp: 628,
+      song: [],
+    },
+    {
+      index: "Act Two",
+      title: "In the Time of Chimpanzees I Was a Monkey",
+      brief:
+        "Host Ira Glass talks to one of the suspects. Ken learns who is behind the bot and who isn’t.",
+      author: "",
+      timestamp: 1992,
+      song: [
+        {
+          raw: "“Where Am I” by Duffy Power",
+          singer: "Duffy Power",
+          name: "Where Am I",
+        },
+        {
+          raw: "“Apeman” by The Kinks",
+          singer: "The Kinks",
+          name: "Apeman",
+        },
+      ],
+    },
+  ],
+};
