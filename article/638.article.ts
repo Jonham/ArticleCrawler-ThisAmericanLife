c657a66b974f3635618f6fd6c747e502e00ec1dc
jonham <jonham@qq.com>
2022-05-24 06:51:26 +0800

    add 638

---
+++ article/638.article.ts
@@ -0,0 +1,60 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 638,
+  "title": "Rom-Com",
+  "brief": "The one thing you know for sure when you're watching a romantic comedy is that it's going to turn out okay in the end. When you're living one? Not so much. This week, stories that unfold like rom-coms.",
+  preUpdateTime: 'February 9, 2018',
+  isUpdate: true,
+  updateTime: 'May 20, 2022',
+  "coverURL": "https://hw4.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/dominic_bugatto-romancefinal.jpg?itok=BsXL1FLI",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/cd9ce857-fd13-4434-87ec-89bc423f571e/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=cd9ce857-fd13-4434-87ec-89bc423f571e",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Romantic comedies usually don’t get much respect.  Producer Neil Drumming explains what’s so great about them. (5 minutes)",
+      "author": "Ira Glass, Neil Drumming",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Meet Cute",
+      "brief": "Actor Daniel Radcliffe reads a short piece of fiction, “The Present,” from Simon Rich’s book of short stories “The Last Girlfriend on Earth.” (10 minutes)",
+      "author": "Simon Rich, Daniel Radcliffe",
+      "timestamp": 297,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "The Obstacle",
+      "brief": "Elna Baker interviews comedian Michelle Buteau about one of her first big romantic challenges.",
+      "author": "Michelle Buteau, Elna Baker",
+      "timestamp": 919,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "The Run",
+      "brief": "David Kestenbaum retraces the steps of Steve Snyder, a man who found himself running for love.",
+      "author": "David Kestenbaum",
+      "timestamp": 1795,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "You Had Me at Hello",
+      "brief": "Comedian Jillian Welsh tells Diane Wu about one of the most romantic—and stressful—nights of her life, a night that paralleled the plot of a rom-com in several ways. A version of this story first appeared on the Risk! podcast.",
+      "author": "Jillian Welsh, Diane Wu",
+      "timestamp": 2508,
+      "song": [
+        {
+          "raw": "“Ko Ko Mo (I Love You So)” by Louis Armstrong and Velma Middleton",
+          "singer": "Louis Armstrong and Velma Middleton",
+          "name": "Ko Ko Mo (I Love You So)"
+        }
+      ]
+    }
+  ]
+}
