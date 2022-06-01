3c250f564a233284a9bade4a1e42d84851632d3d
Jonham.Chen <me@jonham.cn>
2022-06-01 22:11:00 +0800

    update 771

---
+++ article/771.article.ts
@@ -0,0 +1,42 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 771,
+  "title": "The Parents Step In",
+  "brief": "Government isn’t doing much to prevent school shootings. So parents are jumping in: parents whose kids have died in mass shootings, in the wake of each shooting. They take practical, effective action — and they get results.",
+  "updateTime": "May 27, 2022",
+  "coverURL": "https://hw2.thisamericanlife.org/sites/default/files/styles/uncropped/public/episodes/images/ap_262271055042.jpg?itok=JK7i6t93",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/de87d383-b3bf-4085-b4c8-4b364006d823/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=de87d383-b3bf-4085-b4c8-4b364006d823",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "There's a whole infrastructure that springs into action when a mass shooting occurs. There's the police and SWAT teams, the Red Cross, the Billy Graham prayer truck, therapy dogs.",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Keep Breathing",
+      "brief": "Sandy and Lonnie’s daughter, Jessi, died back in 2012, when a gunman opened fire in a movie theater in Aurora, Colo. Since then, they’ve organized their whole lives to be able to reach out to other parents like themselves.",
+      "author": "Miki Meek",
+      "timestamp": 116,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Down the Rabbit Hole",
+      "brief": "Lenny Pozner’s son, Noah, was killed at Sandy Hook. In the years after his death, Lenny and his family were harassed by people who believed the shooting at Sandy Hook never happened – that it was all a conspiracy.",
+      "author": "Miki Meek",
+      "timestamp": 1503,
+      "song": [
+        {
+          "raw": "“The Water” by Johnny Flynn feat. Laura Marling",
+          "singer": "Johnny Flynn feat. Laura Marling",
+          "name": "The Water"
+        }
+      ]
+    }
+  ]
+}
