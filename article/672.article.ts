4fef34949e60ed0daa08357e0d0d5126da1e50ff
Jonham.Chen <me@jonham.cn>
2022-06-13 23:04:09 +0800

    add 672

--- article/672.article.ts
+++ article/672.article.ts
@@ -4,9 +4,11 @@ export const info: ArticleProto ={
   "number": 672,
   "title": "No Fair!",
   "brief": "Stories of very small injustices and also one very big one.",
-  "updateTime": "April 5, 2019",
+  "preUpdateTime": "April 5, 2019",
+  updateTime: 'June 10, 2022',
+  isUpdate: true,
   "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/836401440-672.jpg?itok=H7pBzrdE",
-  "audioURL": "https://stream.thisamericanlife.org/672/QJx9mMe6PRAqJAnVWKzV6GmyT1vjZPs8iC_w1sZvE3c/672.mp3",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/5a12fc83-9bf1-40df-8726-aef5f7610917/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=5a12fc83-9bf1-40df-8726-aef5f7610917",
   "acts": [
     {
       "index": "Prologue",
@@ -19,7 +21,7 @@ export const info: ArticleProto ={
     {
       "index": "Act One",
       "title": "Hoop Reams",
-      "brief": "Writer Michael Lewis takes us inside the world of NBA refereeing. He explains how protests about ;unfair calls have increased in recent years.",
+      "brief": "Writer Michael Lewis takes us inside the world of NBA refereeing. He explains how protests about unfair calls have increased in recent years.",
       "author": "Michael Lewis",
       "timestamp": 795,
       "song": []
@@ -27,7 +29,7 @@ export const info: ArticleProto ={
     {
       "index": "Act Two",
       "title": "The Fairer Sex",
-      "brief": "When Heidi Schreck was 15 years old she loved the United States Constitution — in part, because ;she believed it enshrined the idea of fairness. She traveled to American Legion posts across the ;country, where she competed in speaking competitions about the Constitution.",
+      "brief": "When Heidi Schreck was 15 years old she loved the United States Constitution — in part, because she believed it enshrined the idea of fairness. She traveled to American Legion posts across the country, where she competed in speaking competitions about the Constitution.",
       "author": "Heidi Schreck",
       "timestamp": 2816,
       "song": [
