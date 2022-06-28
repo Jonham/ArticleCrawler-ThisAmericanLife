ad47e1f7f300552d15094e16bd99bf30d0d09758
Jonham.Chen <me@jonham.cn>
2022-06-28 21:02:22 +0800

    add 583

---
+++ article/583.article.ts
@@ -0,0 +1,54 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 583,
+  "title": "It’ll Make Sense When You’re Older",
+  "brief": "Kids do not like being told it’ll make sense when they’re older. They’re pretty sure the grown-ups are wrong.\nView episode details",
+  "updateTime": "June 24, 2022",
+  isUpdate: true,
+  preUpdateTime: "March 25, 2016",
+  "coverURL": "https://hw4.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/583.png?itok=_hFRsWL4",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/b7202006-b59f-4a39-937f-1358ca9e5ac9/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=b7202006-b59f-4a39-937f-1358ca9e5ac9",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Kids do not like getting told it’ll make sense when they’re older. They’re pretty sure the grown-ups are wrong, and whatever the conversation is, they’re up for it.",
+      "author": "Chana Joffe-Walt",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Adolescence",
+      "brief": "Reporter Hillary Frank finds out there is this tradition going on in her town, where big kids take over younger kids’ parties—and she investigates how one kid goes from freaked out to an instigator.",
+      "author": "Hillary Frank",
+      "timestamp": 259,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Grown",
+      "brief": "Comedian Sasheer Zamata does this joke about her mom in her standup act. About how her mom hates white people.",
+      "author": "Sasheer Zamata",
+      "timestamp": 869,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Middle Age",
+      "brief": "Host Chana Joffe-Walt worries she’ll have regrets in 20 years. So she finds someone 20 years older than she is to gauge how bad it gets.",
+      "author": "Chana Joffe-Walt",
+      "timestamp": 2193,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "Old Age",
+      "brief": "For those in the early stages of dementia, some simple tasks become very complex. Chana sits down with one guy determined to figure out why something that used to be so easy has become so hard.",
+      "author": "Chana Joffe-Walt",
+      "timestamp": 2512,
+      "song": []
+    }
+  ]
+}
