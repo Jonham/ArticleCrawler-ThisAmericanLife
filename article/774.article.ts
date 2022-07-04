366fae68eb0d0b95e5fa2fd2a61195e96dcf92af
Jonham.Chen <me@jonham.cn>
2022-07-04 21:38:15 +0800

    add 774 and add folder

---
+++ article/774.article.ts
@@ -0,0 +1,82 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 774,
+  "title": "The Pink House at the Center of the World",
+  "brief": "The Supreme Court case that overturned Roe v. Wade began with a lawsuit filed by a Mississippi abortion clinic. On the day Roe was overturned, we were there. Stories from the center of this moment of history, the day it happened.",
+  "updateTime": "July 1, 2022",
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-774-pinkhouse-gracialam.jpg?itok=vCoyxs6u",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/f189d8bb-d1af-4dfd-b38c-efe726edac92/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=f189d8bb-d1af-4dfd-b38c-efe726edac92",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Host Ira Glass reminds us that, before they change the country, all major Supreme Court cases start with just a person, in a place. (3 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "The Pink House",
+      "brief": "Documentarian Maisie Crow has been following the fight to stay open by the Jackson Women’s Health Clinic, for ten years. Now the Supreme Court decision is forcing their doors shut for good.",
+      "author": "Maisie Crow",
+      "timestamp": 170,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Welcome to Illinois—Abortion Oasis",
+      "brief": "Our producer Zoe Chace goes to Fairview Heights, Illinois– where Planned Parenthood has opened a massive new abortion clinic just across the river from Missouri. Abortion is banned in MIssouri and lots of the surrounding states now that Roe has been overturned.",
+      "author": "Zoe Chace",
+      "timestamp": 1384,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "The Aboriton Desert Across the River",
+      "brief": "Zoe Chace spends time with a lawmaker in Missouri on the day Roe fell who’d worked her whole life for this moment and can’t believe it’s really here.",
+      "author": "Zoe Chace",
+      "timestamp": 1683,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "Next",
+      "brief": "Host Ira Glass walks through possible next steps with a pro-life activist who worked on the Texas SB8 bill, that set a precedent for enforcement of abortion bans throughtout the country.",
+      "author": "Ira Glass",
+      "timestamp": 1984,
+      "song": []
+    },
+    {
+      "index": "Act Five",
+      "title": "The Pill Smuggler",
+      "brief": "Contributor Rebecca Grant introduces us to an abortion pill smuggler.",
+      "author": "Rebecca Grant",
+      "timestamp": 2575,
+      "song": []
+    },
+    {
+      "index": "Act Six",
+      "title": "The Babies ",
+      "brief": "Contributor Emma Green spends time with Anja Baker, who’s working on preparing Mississippi for the influx of babies it will have to absorb now that aboriton is illegal in that state.",
+      "author": "Emma Green",
+      "timestamp": 3134,
+      "song": []
+    },
+    {
+      "index": "Act Seven",
+      "title": "Back to Jackson",
+      "brief": "Maisie Crow is back to take us inside the Jackson clinic once again, as they rally themselves to see all the patients they’d scheduled for the next month to come in in the next ten days, before the ban goes into effect.",
+      "author": "Maisie Crow",
+      "timestamp": 3687,
+      "song": [
+        {
+          "raw": "“When My Morning Comes Around” by Iris Dement, performed by Nikki Lane",
+          "singer": "Iris Dement, performed",
+          "name": "When My Morning Comes Around"
+        }
+      ]
+    }
+  ]
+}
