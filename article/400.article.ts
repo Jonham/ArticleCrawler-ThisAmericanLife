6922716b46123eae2727c58c7356c5fcee56e3fb
Jonham.Chen <me@jonham.cn>
2022-07-18 21:26:20 +0800

    add 400

---
+++ article/400.article.ts
@@ -0,0 +1,74 @@
+import { ArticleProto } from "../parseContent";
+
+export const info: ArticleProto = {
+  number: 400,
+  title: "Stories Pitched by Our Parents",
+  brief:
+    'We try something harder than anything we\'ve ever tried before, by taking the random ideas that members of our own families have told us would be "perfect for the show," and turning them into actual stories.',
+  updateTime: "July 15, 2022",
+  isUpdate: true,
+  preUpdateTime: "February 12, 2010",
+  coverURL:
+    "https://hw1.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/istock-899173316_0.jpg?itok=Cd01PtEK",
+  audioURL:
+    "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/dccdd7be-5957-4080-8831-2c2a9ed13b7a/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=dccdd7be-5957-4080-8831-2c2a9ed13b7a",
+  acts: [
+    {
+      index: "Prologue",
+      title: "Prologue",
+      brief:
+        "Host Ira Glass talks to his dad and stepmom about a story his dad thought might be good for the radio, concerning a mishap with a lost suit on a train. Ira then gathers the producers and announces a contest: Whoever makes the best story out of their parent's pitch wins.",
+      author: "Ira Glass",
+      timestamp: 0,
+      song: [],
+    },
+    {
+      index: "Act One",
+      title: "Lisa's Mom's Story",
+      brief: "Funny Funerals.",
+      author: "Lisa Pollak",
+      timestamp: 405,
+      song: [],
+    },
+    {
+      index: "Act Two",
+      title: "Nancy's Dad's Story",
+      brief: "The Erie Canal.",
+      author: "Nancy Updike",
+      timestamp: 1136,
+      song: [],
+    },
+    {
+      index: "Act Three",
+      title: "Alex's Dad's Story",
+      brief: "Corporate Personhood.",
+      author: "Alex Blumberg",
+      timestamp: 1485,
+      song: [],
+    },
+    {
+      index: "Act Four",
+      title: "Robyn's Dad's Story",
+      brief: "One Switch.",
+      author: "Robyn Semien",
+      timestamp: 1874,
+      song: [],
+    },
+    {
+      index: "Act Five",
+      title: "Alissa's Mom And Sarah's Mom",
+      brief: "Two short ideas that didn't work out so well as full stories.",
+      author: "Sarah Koenig, Julie Snyder",
+      timestamp: 2392,
+      song: [],
+    },
+    {
+      index: "Act Six",
+      title: "Jane's Dad's Story",
+      brief: "Harry Brakeman University.",
+      author: "Jane Feltes",
+      timestamp: 2609,
+      song: [],
+    },
+  ],
+};
