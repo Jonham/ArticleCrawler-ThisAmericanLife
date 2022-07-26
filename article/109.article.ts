f38d436f20fb6e2467f03d27e8c0e39783d5984e
Jonham.Chen <me@jonham.cn>
2022-07-26 21:35:45 +0800

    update 109

---
+++ article/109.article.ts
@@ -0,0 +1,80 @@
+import { ArticleProto } from "../parseContent";
+
+export const info: ArticleProto = {
+  number: 109,
+  title: "Notes on Camp",
+  brief:
+    "People who love summer camp say that non-camp people simply don't understand what's so amazing about it. We attempt to bridge this gap of misunderstanding between camp people and non-camp people.",
+  updateTime: "July 22, 2022",
+  isUpdate: true,
+  preUpdateTime: "August 28, 1998",
+  coverURL:
+    "https://hw1.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/michela_buttignol_tal_notes_on_camp_final.jpg?itok=UHliFxLl",
+  audioURL:
+    "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/4dbce640-7137-4cda-9615-91b46923e6a1/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=4dbce640-7137-4cda-9615-91b46923e6a1",
+  acts: [
+    {
+      index: "Prologue",
+      title: "Prologue",
+      brief:
+        "Camp kids explain how their non-camp friends and their non-camp loved ones have no idea why camp is the most important thing in their lives. Most of this hour takes place at a pair of camps in Michigan—Lake of the Woods, a girls camp, and Greenwoods, a boys camp.",
+      author: "Ira Glass",
+      timestamp: 0,
+      song: [],
+    },
+    {
+      index: "Act One",
+      title: "Mr. Popular",
+      brief:
+        "David Himmel is a college sophomore and a former camper who became a counselor. He says all the best experiences of his life have been at camp or with camp people.",
+      author: "Ira Glass",
+      timestamp: 161,
+      song: [],
+    },
+    {
+      index: "Act Two",
+      title: "Fear",
+      brief:
+        "Sure, kids today are sophisticated. But get them living in the woods for a few days, and they revert to some of the most stupidly primitive forms of entertainment known since the dawn of civilization.",
+      author: "Ira Glass",
+      timestamp: 629,
+      song: [],
+    },
+    {
+      index: "Act Three",
+      title: "You",
+      brief:
+        "We asked listeners to call and write with their camp stories. Hundreds did.",
+      author: "Listeners Like You",
+      timestamp: 1068,
+      song: [],
+    },
+    {
+      index: "Act Four",
+      title: "Ritual",
+      brief:
+        "Camp Lake of the Woods holds a fake Indian powwow during the summer. This kind of fake Native American-ness has been a part of camping in America since organized camping began a century ago.",
+      author: "Ira Glass",
+      timestamp: 1495,
+      song: [],
+    },
+    {
+      index: "Act Five",
+      title: "How The Israeli Army Is Just Like Summer Camp",
+      brief:
+        "NPR's Adam Davidson tells a true story from his own childhood. He was sent to a camp run by the Israeli army at its own training facility.",
+      author: "Adam Davidson",
+      timestamp: 1786,
+      song: [],
+    },
+    {
+      index: "Act Six",
+      title: "Color Days",
+      brief:
+        "**This American Life** producer Julie Snyder reports on a three-day competition called \"Color Days.\" It's most kids' favorite time at camp — despite the fact that the girls, at least, spend most of the three days crying and screaming. It's thrilling to be part of a team at this level of intensity.",
+      author: "Julie Snyder",
+      timestamp: 2265,
+      song: [],
+    },
+  ],
+};
