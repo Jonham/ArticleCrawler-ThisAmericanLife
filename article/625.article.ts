24a9aafbff348606b62789fa59e369f98ed227cd
Jonham.Chen <me@jonham.cn>
2022-08-16 08:10:17 +0800

    add 777

---
+++ article/625.article.ts
@@ -0,0 +1,44 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 625,
+  "title": "Essay B",
+  "brief": "In 1967, the first two Black students were enrolled at an all-white private boarding school in Virginia. The main reason they were there? To benefit the white kids.",
+  "updateTime": "August 5, 2022",
+  isUpdate: true,
+  preUpdateTime: "September 8, 2017",
+  "coverURL": "https://hw1.thisamericanlife.org/sites/default/files/styles/portrait/public/episodes/images/625-portrait.png?itok=uYEIebcG",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/8a450aed-ce21-4453-8062-1726df80d075/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=8a450aed-ce21-4453-8062-1726df80d075",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Host Ira Glass talks to Mariya Karimjee about a college application essay question. Essay B asks students to imagine a person they might meet in college—someone from a very different background.",
+      "author": "Mariya Karimjee",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "How to Win Friends and Influence White People",
+      "brief": "Back in the late 1960s, a wealthy tobacco heiress saw that integration was happening all around the country—except at prep schools in the South. So she set out to find the best Black students in neighborhood public schools—in hopes of teaching the white prep-school students to be less bigoted. Mosi Secret tells the story of how the first two Black students to integrate Virginia Episcopal School succeeded beyond anyone’s expectations.",
+      "author": "Mosi Secret",
+      "timestamp": 918,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Act Two",
+      "brief": "Mosi Secret’s story continues. We find out about a Black student who struggled at VES.",
+      "author": "",
+      "timestamp": 2598,
+      "song": [
+        {
+          "raw": "“Maybe It’ll Rub Off” by Tower of Power",
+          "singer": "Tower of Power",
+          "name": "Maybe It’ll Rub Off"
+        }
+      ]
+    }
+  ]
+}
---
+++ article/777.article.ts
@@ -0,0 +1,84 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 777,
+  "title": "Name. Age. Detail.",
+  "brief": "Ten people were killed at a grocery store in Buffalo, NY. Their stories, as you’ve never heard them.",
+  "updateTime": "August 12, 2022",
+  "coverURL": "https://hw1.thisamericanlife.org/sites/default/files/styles/uncropped/public/episodes/images/20220520_buffalo-tops-171a_mustafahussain.jpeg?itok=r9X2ZG6T",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/a3108513-928a-466f-8575-de9edd70a8b1/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=a3108513-928a-466f-8575-de9edd70a8b1",
+  "acts": [
+    {
+      "index": "Pearl Young and Aaron Salter Jr.",
+      "title": "Pearl Young and Aaron Salter Jr.",
+      "brief": "A person cannot be summarized in a sentence.",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act 1",
+      "title": "Celestine Chaney",
+      "brief": "Kayla Jones was extremely close with her Grandmother Celestine Chaney. **For Colored Nerds** host Brittany Luse talks with Kayla about her grandmother’s shared love of beauty. (7 minutes)",
+      "author": "",
+      "timestamp": 491,
+      "song": []
+    },
+    {
+      "index": "Act 2",
+      "title": "Andre Mackniel",
+      "brief": "Bim Adewumni finds a window into Andre Mackneil’s world through TikTok.  (4 minutes)",
+      "author": "",
+      "timestamp": 920,
+      "song": []
+    },
+    {
+      "index": "Act 3",
+      "title": "Katherine Massey ",
+      "brief": "The fact that was repeated about Katherine Massey was that she had written an letter to the newspaper calling for gun control the year before she was murdered. Katherine Massey made things happen. Eve L.",
+      "author": "",
+      "timestamp": 1161,
+      "song": []
+    },
+    {
+      "index": "Act 4",
+      "title": "Margus Morrison",
+      "brief": "Margus Morrison was a school bus aide, father of six.",
+      "author": "",
+      "timestamp": 1955,
+      "song": []
+    },
+    {
+      "index": "Act 5",
+      "title": "Geraldine Chapman Talley",
+      "brief": "Writer Kiese Laymon wanted to talk through one fact in particular about Geraldine Chapman Talley’s life: her move from Alabama to Buffalo.",
+      "author": "",
+      "timestamp": 2065,
+      "song": []
+    },
+    {
+      "index": "Act 6",
+      "title": "Ruth Whitfield",
+      "brief": "Ruth Whitfield. 86 years old. There was a detail repeated often came to Ruth Whitfield, the oldest victim.",
+      "author": "",
+      "timestamp": 2659,
+      "song": []
+    },
+    {
+      "index": "Act 7",
+      "title": "Roberta Drury",
+      "brief": "She was the first person and the youngest person killed. She’s described in a lot of stories as vibrant, funny, joyful. Damon Young was struck by another particular detail.",
+      "author": "",
+      "timestamp": 3207,
+      "song": []
+    },
+    {
+      "index": "Act 8",
+      "title": "Deacon Heyward Patterson",
+      "brief": "Heyward Patterson was a deacon at the State Tabernacle Church of God in Christ. He was at Tops often where he worked as a jitney driver.",
+      "author": "",
+      "timestamp": 3493,
+      "song": []
+    }
+  ]
+}
