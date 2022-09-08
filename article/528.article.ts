b8fa5f1ab1fbdab025cb29c75fb286c1ab2bf02c
Jonham.Chen <me@jonham.cn>
2022-09-09 07:18:42 +0800

    update 528

---
+++ article/528.article.ts
@@ -0,0 +1,54 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 528,
+  "title": "The Radio Drama Episode",
+  "brief": "Our most ambitious live show ever! We pulled together a massive team of theater pros at the Brooklyn Academy of Music's Opera House—nearly 50 singers, actors, dancers and musicians. The result? Journalism turned into a Broadway musical, into opera. Mike Birbiglia, Sasheer Zamata, Stephin Merritt, Josh Hamilton, Lindsay Mendez, Lin-Manuel Miranda and others.",
+  "preUpdateTime": "June 20, 2014",
+  "updateTime": "September 2, 2022",
+  isUpdate: true,
+  "coverURL": "https://hw1.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/528.jpg?itok=JfQNp7y0",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/0c2ae293-d05c-4d14-827a-97d92b82763d/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=0c2ae293-d05c-4d14-827a-97d92b82763d",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Carin Gilfry explains how she once accidentally locked herself in a hotel closet, and because today’s show is being broadcast from an opera house stage, Ira is able to take the story to a place he never usually can.",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "21 Chump Street",
+      "brief": "Lin-Manuel Miranda turns a piece of reporting we broadcast in 2012, into a 14-minute Broadway mini-musical, created by people who normally work on Broadway.",
+      "author": "Lin-Manuel Miranda",
+      "timestamp": 1234,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Of Mice and Men",
+      "brief": "Comedian Mike Birbiglia, his wife, and his cat take a trip together and meet some parasitic zombie mice.",
+      "author": "Mike Birbiglia",
+      "timestamp": 2306,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "How Do You Slow This Thing Down",
+      "brief": "Joshuah Bearman tells a story that’s a sequel to his memorable episode about his mother and half-brother David. It’s done onstage as a play that’s structured like a radio documentary, with Josh Hamilton playing Joshuah, and James Ransone playing his brother.",
+      "author": "Josh Bearman",
+      "timestamp": 2928,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "Bus! Stop!",
+      "brief": "Comedian Sasheer Zamata stages a radio play, complete with sound effects and comedians Nicole Byer, Chris Gethard, and Frank Garcia Hejl. It’s a true story about a recent bus accident.",
+      "author": "Sasheer Zamata",
+      "timestamp": 4313,
+      "song": []
+    }
+  ]
+}
