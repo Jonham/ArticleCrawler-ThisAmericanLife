afbcf901ed89191d27efe9eefba72c8b50fd236f
Jonham.Chen <me@jonham.cn>
2022-09-14 22:52:12 +0800

    add 550

---
+++ article/550.article.ts
@@ -0,0 +1,44 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 550,
+  "title": "Three Miles",
+  "brief": "There’s a program that brings together kids from two schools. One school is public and in the country’s poorest congressional district. The other is private and costs $43,000/year. They are three miles apart. The hope is that kids connect, but some of the public school kids just can’t get over the divide. We hear what happens when you get to see the other side and it looks a lot better.",
+  "preUpdateTime": "March 13, 2015",
+  "updateTime": "September 9, 2022",
+  isUpdate: true,
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/keith_negley_3miles_final.jpg?itok=q58IiH7q",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/8eb7e27b-99d5-44c1-8048-d3e9d58d0124/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=8eb7e27b-99d5-44c1-8048-d3e9d58d0124",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira Glass introduces producer Chana Joffe-Walt, who reports this week's story.",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Act One",
+      "brief": "Chana Joffe-Walt tells what happened when a group of public school students in the Bronx went to visit an elite private school three miles away.",
+      "author": "Chana Joffe-Walt",
+      "timestamp": 45,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Act Two",
+      "brief": "The kids who traveled three miles up the road are in their mid-20s now. We hear how what they saw affected them for years, including at college.",
+      "author": "",
+      "timestamp": 2176,
+      "song": [
+        {
+          "raw": "“How The Other Half Lives” by Jack Marks",
+          "singer": "Jack Marks",
+          "name": "How The Other Half Lives"
+        }
+      ]
+    }
+  ]
+}
