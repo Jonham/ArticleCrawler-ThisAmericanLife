c5b19e9b7c96639bd3e6b01edefc71e18426a364
Jonham.Chen <me@jonham.cn>
2022-09-21 08:36:44 +0800

    update 779

---
+++ article/779.article.ts
@@ -0,0 +1,42 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 779,
+  "title": "Ends of the Earth",
+  "brief": "An exploration of the very upper limits of what you do for someone you love.",
+  "updateTime": "September 16, 2022",
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/istock-576547100.jpg?itok=5fxXwtbo",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/31bb8c02-c572-4380-93fd-99f628ba125e/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=31bb8c02-c572-4380-93fd-99f628ba125e",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Host Ira Glass explains what we’re doing on today’s show. (2 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Exit Strategy",
+      "brief": "Amy Bloom tells the story of her husband, Brian, getting Alzheimer's and wanting assisted suicide. Her search to find a way to do that led her to Dignitas, in Switzerland.",
+      "author": "",
+      "timestamp": 145,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Comedy Duo",
+      "brief": "Comedian Zarna Garg tells jokes onstage about the extreme ways she tries to control her daughter Zoya’s life.",
+      "author": "",
+      "timestamp": 2787,
+      "song": [
+        {
+          "raw": "“I’ll Do Anything For You” by Denroy Morgan",
+          "singer": "Denroy Morgan",
+          "name": "I’ll Do Anything For You"
+        }
+      ]
+    }
+  ]
+}
