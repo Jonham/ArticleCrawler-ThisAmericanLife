2477bc40a825b081ebce178342a2d5d45edce7d7
Jonham <jonham@qq.com>
2022-10-12 08:47:04 +0800

    add 781

---
+++ article/781.article.ts
@@ -0,0 +1,42 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 781,
+  "title": "Watching the Watchers",
+  "brief": "People taking it upon themselves to keep an eye on those in charge.",
+  "updateTime": "October 7, 2022",
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-781-watchingthewatchers-ellisrosen.jpg?itok=-2yCk5dt",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/9d1adbf0-b9cc-46ad-8854-81628c805241/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=9d1adbf0-b9cc-46ad-8854-81628c805241",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Host Ira Glass looks into what’s going on behind the scenes of this year’s midterm election.",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Magazona",
+      "brief": "Washington Post reporter, Isaac Arnsdorf, and producer, Zoe Chace, continue the story about the takeover of the Republican party. Together, they hit the road to document how the presence of the MAGA newbies are changing things on the ground in Arizona. Isaac is writing a book all about the MAGA plot to take over America.",
+      "author": "",
+      "timestamp": 315,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "I’ll Be Watching You",
+      "brief": "A group of 6th grade boys are bothered by their teacher’s behavior. They complain, but no one listens.",
+      "author": "Ira Glass",
+      "timestamp": 2611,
+      "song": [
+        {
+          "raw": "“Who’s Watching the Watchers ” by LaBelle",
+          "singer": "LaBelle",
+          "name": "Who’s Watching the Watchers "
+        }
+      ]
+    }
+  ]
+}
