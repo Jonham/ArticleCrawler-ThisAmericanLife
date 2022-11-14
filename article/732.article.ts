75b1b3a4e2c815b4ff0235d13ad5bdba9d0431a8
Jonham.Chen <jonham.chen@yichang.io>
2022-11-14 20:50:59 +0800

    add 732

---
+++ article/732.article.ts
@@ -0,0 +1,52 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 732,
+  "title": "Secrets",
+  "brief": "Why we tell them, and what happens after we do.",
+  "preUpdateTime": "February 19, 2021",
+  isUpdate: true,
+  "updateTime": "November 11, 2022",
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/secrets-bed.png?itok=NVs8MjGc",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/fcc546b4-abdc-4e0d-bf58-61b7138fc0b3/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=fcc546b4-abdc-4e0d-bf58-61b7138fc0b3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Guest host Susan Burton watches videos of female legislators sharing very private stories.",
+      "author": "Susan Burton",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "My Own Very Serious Comedy About Women and Food",
+      "brief": "Susan Burton talks to women about why it’s so hard to talk about eating disorders.",
+      "author": "Susan Burton",
+      "timestamp": 426,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Mommy’s Busy Right Now",
+      "brief": "Lana has a problem lots of mothers have right now during the pandemic—but a more extreme version of it.",
+      "author": "Lina Misitzis",
+      "timestamp": 2060,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Bad Cell Service",
+      "brief": "Pavan Bivigou writes about having sickle-cell anemia.",
+      "author": "Pavan Bivigou",
+      "timestamp": 2784,
+      "song": [
+        {
+          "raw": "“Secrets” by Mary Lambert",
+          "singer": "Mary Lambert",
+          "name": "Secrets"
+        }
+      ]
+    }
+  ]
+}
