a4cf2d6c7d95c996b08eb232ea5488c4a059e3f7
Jonham.Chen <jonham.chen@yichang.io>
2022-12-05 22:13:34 +0800

    add 786

---
+++ article/786.article.ts
@@ -0,0 +1,44 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 786,
+  "title": "It's a Game Show!",
+  "brief": "Something we’ve never done before: true stories told in the form of a game show.",
+  "updateTime": "December 2, 2022",
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/uncropped/public/episodes/images/gettyimages-155668744.jpg?itok=p-VRhag0",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/ac35215c-11f7-4e1d-be94-099843a50116/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=ac35215c-11f7-4e1d-be94-099843a50116",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Jiayang Fan has this theory that because she's spent so much time thinking about her own accent when she speaks English, she believes that when she hears other Chinese-Americans speak, she can tell how old they were when they immigrated to the U.S. (7 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "With Great Power Comes Great Pronounceability",
+      "brief": "We test Jiayang Fan’s self-proclaimed special skill by having her guess the age that three Chinese-Americans came to the U.S. after listening to them talk.",
+      "author": "",
+      "timestamp": 466,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "You Bet Your Planet’s Life!",
+      "brief": "What would it take for the U.S. to reach its goals set out by the Paris Agreement? What steps would we have to take to cut emissions by 50% by 2030? We challenge climate researcher Melissa Lott to get us to that number.",
+      "author": "",
+      "timestamp": 1796,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "You Didn’t Hear It From Me. Really. You Didn’t.",
+      "brief": "A game of telephone played on the podcast **Normal Gossip** reveals how gossip spreads, and why stories change from person to person.",
+      "author": "",
+      "timestamp": 2580,
+      "song": []
+    }
+  ]
+}
