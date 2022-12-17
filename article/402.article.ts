9ed796df7016a41493d3f299b83f3267b31ecb51
Jonham.Chen <jonham.chen@yichang.io>
2022-12-17 11:55:15 +0800

    add 402 and task

---
+++ article/402.article.ts
@@ -0,0 +1,44 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 402,
+  "title": "Save the Day",
+  "brief": "Stories about one person single-handedly taking charge of a situation gone wrong.",
+  "preUpdateTime": "March 12, 2010",
+  isUpdate: true,
+  "updateTime": "December 9, 2022",
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/istock-1181321912-c.jpg?itok=vaHFO8jB",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/4504d91c-c68e-4001-b454-7e0c223734ac/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=4504d91c-c68e-4001-b454-7e0c223734ac",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "When Luke Davies was 11 years old in Australia, his family moved from the boring suburbs to an incredibly fun place: a tourist park full of attractions, where his dad had gotten a job. There, he was considered kind of a wimpy kid, until he got his chance to save the day.",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Midlife Cowboy",
+      "brief": "James Spring had hit his late 30s, and found his life utterly unremarkable. He needed to do something big.",
+      "author": "James Spring",
+      "timestamp": 706,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "I'd Like To Spank The Academy",
+      "brief": "For the last 13 years, the University of Montevallo in Alabama has held an event called \"The Life Raft Debate,\" where several professors take the stage and each tries to convince the students that his or her discipline—chemistry, say, or communications—is the most essential field of study. But in 2007, a professor named Jon Smith decided that the debate itself needed saving.",
+      "author": "",
+      "timestamp": 2502,
+      "song": [
+        {
+          "raw": "“What More Can I Do?” by Naomi Shelton & the Gospel Queens",
+          "singer": "Naomi Shelton & the Gospel Queens",
+          "name": "What More Can I Do?"
+        }
+      ]
+    }
+  ]
+}
