0b6297f06a82ea2d9351baa4acc7adbbde0b4067
Jonham.Chen <jonham.chen@yichang.io>
2022-12-28 22:50:00 +0800

    add 787

---
+++ article/787.article.ts
@@ -0,0 +1,52 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 787,
+  "title": "Baby's First Christmas",
+  "brief": "People experiencing Christmas in brand new ways, giving the holiday even more meaning.",
+  "updateTime": "December 23, 2022",
+  "coverURL": "https://hw1.thisamericanlife.org/sites/default/files/styles/portrait/public/episodes/images/tal-santa-allen-christmasshow_0.png?itok=fQBRQp-7",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/eb9e614c-8bea-44d1-9ac6-794268ef2848/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=eb9e614c-8bea-44d1-9ac6-794268ef2848",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira introduces us to Santa Allen. A man who is NOT the real Santa Claus but who will – for the first time ever – be sitting in a chair, dressed as Santa, asking little children what they want for Christmas this year.  He’s nervous.",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Does Santa Believe in Himself?",
+      "brief": "Producer Aviva DeKornfeld accompanies Santa Allen to his first “chair gig” to see whether all of his hopes, or all of his fears, or a little of both, will be realized. (16 minutes)",
+      "author": "Aviva DeKornfeld",
+      "timestamp": 250,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Jews for Jesus, Just for One Day ",
+      "brief": "Alex Edelman grew up in an Orthodox Jewish household in Boston. But one year, unexpectedly, his family decided to celebrate Christmas – for what he says are very Jewish reasons.",
+      "author": "",
+      "timestamp": 1232,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Buffalo Girls Can’tcha Come Out Tonight ",
+      "brief": "Our former colleague Neil Drumming has never seen “It’s a Wonderful Life” – which makes no sense given that he’s seen almost every other movie there is.",
+      "author": "",
+      "timestamp": 2148,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "Single Bells ",
+      "brief": "Scaachi Koul is commemorating a Christmas first of her own – and ON her own – this year.",
+      "author": "Scaachi Koul",
+      "timestamp": 2763,
+      "song": []
+    }
+  ]
+}
