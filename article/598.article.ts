cd298345d02899b28f032b0c7d686e06664d400e
Jonham.Chen <jonham.chen@yichang.io>
2023-01-02 21:25:03 +0800

    add 598

---
+++ article/598.article.ts
@@ -0,0 +1,46 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 598,
+  "title": "My Undesirable Talent",
+  "brief": "San Francisco’s Spider-Man burglar was remarkable. He dropped into buildings from skylights, leapt 10 feet from one roof to another. But mostly, his talent got him into trouble. This week, his story, and stories of other undesirable talents.",
+  "updateTime": "December 30, 2022",
+  "preUpdateTime": "October 7, 2016",
+  isUpdate: true,
+  "coverURL": "https://hw4.thisamericanlife.org/sites/default/files/styles/portrait/public/episodes/images/598.jpg?itok=fSAYNCB7",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/7ac9d0b2-7669-4abd-9ffb-624f4ff7b0d8/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=7ac9d0b2-7669-4abd-9ffb-624f4ff7b0d8",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira talks to Biba Struja – a Serbian man who says that when he was in high school, he discovered that he seemed to have a high resistance to electricity. It’s a power that he’s utilized, but is mostly a curse.",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Climb Spree",
+      "brief": "Awhile back, San Francisco experienced a rash of burglaries that all fit a pattern. The suspect got into businesses at night through skylights, or the attic, and then lowered himself in with ropes where he would rob safes.",
+      "author": "Karen Duffin",
+      "timestamp": 305,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Uganda Be Kidding Me",
+      "brief": "Zora Bikangaga grew up in a mostly white California suburb, the son of Ugandan immigrants. But when he went to college, someone thought he himself was Ugandan.",
+      "author": "Neil Drumming",
+      "timestamp": 1773,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Disbarred",
+      "brief": "Wendy Whelan was a principal dancer with the New York City Ballet until 2014. She explains an undesirable side of having an incredible talent.",
+      "author": "Ira Glass",
+      "timestamp": 3249,
+      "song": []
+    }
+  ]
+}
