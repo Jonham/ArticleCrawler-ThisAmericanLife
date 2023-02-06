b74ade23015ff166ac00651f1be988d2b3c38fac
Jonham.Chen <jonham.chen@yichang.io>
2023-02-06 09:52:35 +0800

    update 542

---
+++ article/542.article.ts
@@ -0,0 +1,52 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 542,
+  "title": "Wait—Do You Have The Map?",
+  "brief": "Feeling lost and trying to figure out how to move ahead.",
+  "preUpdateTime": "December 12, 2014",
+  isUpdate: true,
+  "updateTime": "February 3, 2023",
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/michela_buttignol_wait_-_do_you_have_the_map-finaljpg.jpg?itok=tB6CePhz",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/488f4a33-081f-4f30-92e4-bf947457c7e6/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=488f4a33-081f-4f30-92e4-bf947457c7e6",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "In New York, like in lots of cities, you can call 3-1-1 for help with city services, but there's lots of things that people call in for that operators don't have answers for. And so they improvise.",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "A Marriage of Inconvenience",
+      "brief": "This next story is about a couple in a relationship that’s unlike the homes either of them grew up in. They’re married.",
+      "author": "Christopher Rhoads",
+      "timestamp": 303,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Final Rest Stop",
+      "brief": "Back in 2013, comedian Tig Notaro went on a tour that was filmed. It was a strange tour.",
+      "author": "Nancy Updike",
+      "timestamp": 1489,
+      "song": [
+        {
+          "raw": "“Small Car” by Marvin Pontiac",
+          "singer": "Marvin Pontiac",
+          "name": "Small Car"
+        }
+      ]
+    },
+    {
+      "index": "Act Three",
+      "title": "Not All Who Wander Are Lost…But Some Definitely Are",
+      "brief": "A car is a classic place to realize: \"oh, I’m lost.\" But sometimes the realization of being lost comes first, and the car is the solution. Drive, keep driving, get un-lost.",
+      "author": "James Spring",
+      "timestamp": 1840,
+      "song": []
+    }
+  ]
+}
