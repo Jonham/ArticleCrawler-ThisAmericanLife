d856a2a4410a771499f7b8adc81838ef8ee47bcc
Jonham.Chen <jonham.chen@yichang.io>
2023-02-14 14:05:19 +0800

    add 791

---
+++ article/791.article.ts
@@ -0,0 +1,72 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 791,
+  "title": "Math or Magic?",
+  "brief": "When it comes to finding love, there seems to be two schools of thought on the best way to go about it. One says, wait for that lightning-strike magic. The other says, make a calculation and choose the best option available. Who has it right?",
+  "updateTime": "February 10, 2023",
+  "coverURL": "https://hw2.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-791-mathormagic-solcotti.jpg?itok=p4ctbZUy",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/8f1c46b1-4a55-44a3-b420-37d1b50a3ec6/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=8f1c46b1-4a55-44a3-b420-37d1b50a3ec6",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "When guest host Tobin Low was looking for a husband, he got opposing advice from two of the most important people in his life, his mom and his best friend.",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "10 Things I Require About You ",
+      "brief": "Zarna Garg had a clear plan for how she was going to find a husband.",
+      "author": "",
+      "timestamp": 475,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Crazy Stupid Love",
+      "brief": "People who fall in love at first sight often describe it as a kind of magic. One of our producers, Aviva DeKornfeld,  is skeptical of these sorts of claims.",
+      "author": "",
+      "timestamp": 1529,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "He’s All That",
+      "brief": "Calvin is an 11 year old who is learning what love is all about,  the hard way. (7 minutes)",
+      "author": "",
+      "timestamp": 2141,
+      "song": [
+        {
+          "raw": "“Breaking Up is Hard to Do” by Neil Sedaka",
+          "singer": "Neil Sedaka",
+          "name": "Breaking Up is Hard to Do"
+        }
+      ]
+    },
+    {
+      "index": "Act Four",
+      "title": "How To Leave a Guy in 10 Days",
+      "brief": "Writer Marie Phillips believes that magic is not just reserved for the beginning of a relationship.",
+      "author": "",
+      "timestamp": 2564,
+      "song": []
+    },
+    {
+      "index": "Coda",
+      "title": "Coda",
+      "brief": "Tobin Low tells us which camp he falls in — math or magic. (2 minutes)",
+      "author": "",
+      "timestamp": 3038,
+      "song": [
+        {
+          "raw": "“Magic” by Pilot",
+          "singer": "Pilot",
+          "name": "Magic"
+        }
+      ]
+    }
+  ]
+}
