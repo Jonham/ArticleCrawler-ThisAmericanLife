72819292a393d40a8cd4aae343f6eaf600b934e1
Jonham.Chen <jonham.chen@yichang.io>
2023-03-29 06:45:35 +0800

    add 794

---
+++ article/794.article.ts
@@ -0,0 +1,48 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 794,
+  "title": "So Close and Yet So Far",
+  "brief": "People ​so close to each other, ​in ​extremely intimate situations​,​ who are also a million miles apart.",
+  "updateTime": "March 24, 2023",
+  "coverURL": "https://hw2.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-794-soclose-madisonketchum.jpg?itok=ZmIWcbVv",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/b7e7b73b-db6e-4252-8790-1d481c9549d1/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=b7e7b73b-db6e-4252-8790-1d481c9549d1",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Valerie Kipnis tells Ira about riding the subway, shoulder-to-shoulder with someone she knows quite well, pretending she doesn’t see him.",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Love and Other Exports",
+      "brief": "How much can you trust whether somebody who you think is close to you really is close to you? Saidu Tejan-Thomas Jr.’s been thinking about that question since a recent visit with some of his childhood friends in Sierra Leone. (37 minutes)",
+      "author": "",
+      "timestamp": 535,
+      "song": [
+        {
+          "raw": "“Tell Me It’s Real” by K-Ci & JoJo",
+          "singer": "K-Ci & JoJo",
+          "name": "Tell Me It’s Real"
+        }
+      ]
+    },
+    {
+      "index": "Act Two",
+      "title": "Fire Sale",
+      "brief": "Comedian Tig Nataro has the story of someone as close as her actual bedside yet who, in another way, is impossibly far away. (9 minutes)To read more about the kids stuck in the sewer, check out Jake Offenhartz’s story in Gothamist.",
+      "author": "",
+      "timestamp": 2906,
+      "song": [
+        {
+          "raw": "“Close to You” by Potsu",
+          "singer": "Potsu",
+          "name": "Close to You"
+        }
+      ]
+    }
+  ]
+}
