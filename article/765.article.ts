32df805c345bbc88097d1fff5d19de96edaf0267
Jonham.Chen <jonham.chen@yichang.io>
2023-04-05 14:15:36 +0800

    add 765

---
+++ article/765.article.ts
@@ -0,0 +1,58 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 765,
+  "title": "Off Course",
+  "brief": "Three people, and one animal, who know the path their lives will take until, suddenly, they don’t.",
+  "preUpdateTime": "March 18, 2022",
+  isUpdate: true,
+  "updateTime": "March 31, 2023",
+  "coverURL": "https://hw3.thisamericanlife.org/sites/default/files/episodes/videos/tal_singleloop.mp4",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/feaf7292-2425-4e69-a5f1-e8aad976f9b3/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=feaf7292-2425-4e69-a5f1-e8aad976f9b3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Like a lot of parents, Yibin Li’s dad dedicated himself to making sure his daughter stayed on a path that would lead her to a better life than his. But the obstacles her dad had to surmount to achieve this are unlike those any parent anywhere has faced.",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "The Audition",
+      "brief": "Ira’s story of Yibin Li continues.",
+      "author": "Ira Glass",
+      "timestamp": 381,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "The Synchronized Swimmers",
+      "brief": "At the age of 17, competitive swimmer Lynne Cox had already accomplished a lot in the open ocean. She’d set two world records crossing the English Channel.",
+      "author": "Phoebe Judge",
+      "timestamp": 1355,
+      "song": [
+        {
+          "raw": "“Lost at Sea” by Oshima Brothers",
+          "singer": "Oshima Brothers",
+          "name": "Lost at Sea"
+        }
+      ]
+    },
+    {
+      "index": "Act Three",
+      "title": "The Year of Manic-al Thinking",
+      "brief": "Comedian Casey Wilson’s mom was a stabilizing force in her family. So after her unexpected death, both Casey and her father felt devastated and unmoored.",
+      "author": "Casey Wilson",
+      "timestamp": 2359,
+      "song": [
+        {
+          "raw": "“Lost My Way” by Bonny Doon",
+          "singer": "Bonny Doon",
+          "name": "Lost My Way"
+        }
+      ]
+    }
+  ]
+}
