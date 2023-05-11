b385719e00523fb9c24602ebe4198ecfdc071f74
Jonham.Chen <jonham.chen@yichang.io>
2023-05-11 10:36:16 +0800

    update

---
+++ article/798.article.ts
@@ -0,0 +1,58 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 798,
+  "title": "Leaving the Fold",
+  "brief": "A week after Jerry Springer’s death, we go back to a story we first broadcast years ago, about a side of Springer most people don’t know and can’t imagine: his years as an idealistic politician in the mold of Bobby Kennedy. Plus other stories of people who try to leave some moment in their life behind, which can be hard.",
+  "preUpdateTime": "",
+  "isUpdate": false,
+  "updateTime": "May 5, 2023",
+  "coverURL": "https://hw1.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/820603011.jpg?itok=kohuuQ7q",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/9042bfe4-7ff1-40c9-8b6a-38f22b1a121e/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=9042bfe4-7ff1-40c9-8b6a-38f22b1a121e",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira explains the premise of this week’s show, where most of the stories were first broadcast in 2004. (3 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "He Contains Multitudes",
+      "brief": "Alex Blumberg tells the true story of Jerry Springer's life before he was a talk show host. It's the story of an idealistic and serious Jerry Springer, a progressive politician, and the most popular mayor ever of a certain American city. (31 minutes)",
+      "author": "Alex Blumberg",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "God and Hockey",
+      "brief": "Ira talks with Shalom Auslander, who was raised as an Orthodox Jew and who made a pivotal break with his faith at a Rangers game. (6 minutes)Shalom Auslander is the author of several books, including Foreskin's Lament where he tells this story, and his latest Mother for Dinner.",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Slapshot” by Ray Castoldi",
+          "name": "“Slapshot”",
+          "singer": "Ray Castoldi"
+        }
+      ]
+    },
+    {
+      "index": "Act Three",
+      "title": "Would You Like to Come Up to First Class?",
+      "brief": "The journalist E. Jean Carroll is in court this week with her rape case against Donald Trump. In 2020 she published a series of stories interviewing women who’ve accused President Trump of sexual assault or harassment. At the time, she felt like these stories had been so widely covered that people had gotten used to them and ignored them. Which seemed sort of incredible to her. Back then she adapted one of the stories for our show and we’re replaying it today, a frank conversation with another one of the president’s accusers, Jessica Leeds, who also testified in Carroll’s case against Trump. (16 minutes)The story is adapted from a series in The Atlantic. E. Jean Carroll is the author of the memoir What Do We Need Men For? and runs a Substack newsletter.",
+      "author": "Susan Burton",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Clean Break” by Jenny Owen Youngs",
+          "name": "“Clean Break”",
+          "singer": "Jenny Owen Youngs"
+        }
+      ]
+    }
+  ]
+}
