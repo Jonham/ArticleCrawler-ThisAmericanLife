0160c79b8ac8382ee9d06baf4557a55d14a651c2
Jonham.Chen <jonham.chen@yichang.io>
2023-06-04 09:38:49 +0800

    update

---
+++ article/800.article.ts
@@ -0,0 +1,46 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 800,
+  "title": "Jane Doe",
+  "brief": "Five years after the #MeToo explosion, what’s happened in the lives of the women who stepped forward and went public with their stories? We tell the story of a teenager who spoke out against one of the most powerful people in her state, and what happened next.",
+  "preUpdateTime": "",
+  "isUpdate": false,
+  "updateTime": "May 26, 2023",
+  "coverURL": "https://hw2.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-800-janedoe-chanelmiller-v1.jpg?itok=OtAZ1sJW",
+  "audioURL": "https://dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/7bac5f58-9ee2-495a-acb0-838ad9fc2b53/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=7bac5f58-9ee2-495a-acb0-838ad9fc2b53",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Some powerful and well known men lost their jobs after #MeToo. But what about the women at the center of all this who’ve been way less visible after they told what happened to them? We hear about big and small ways the aftermath of coming forward continues to pop up in their daily lives. (10 minutes)Jules Woodson’s nonprofit is Help; Hear; Heal. Tanya Selvaratnam is the author of the memoir Assume Nothing: A Story of Intimate Violence.",
+      "author": "Ira GlassValerie Kipnis",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "The Intern",
+      "brief": "Back in 2021, a 19-year-old intern at the Idaho state legislature reported that a state Representative named Aaron von Ehlinger raped her. She went by the name Jane Doe. There was a public ethics hearing and Ehlinger resigned. State legislators talked about how proud they were of their ability to do the right thing so quickly. But the story that the public knows is very different from what actually happened to Jane. She talks about it in-depth for the first time. (25 minutes)",
+      "author": "Miki Meek",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "The Witness",
+      "brief": "Jane Doe walks into a public ethics hearing at the Idaho state capitol and navigates the aftermath. (23 minutes)Song: “Here We Have Idaho” \nFiona Apple (vocals)\nAmy Wood (drums and percussion)\nDavid Garza (acoustic guitar, electric guitar, piano, vocals)\nSabastian Steinberg (bass, electric autoharp, vocals)\nJohn Would (tremolo guitar)\nEngineered by John Would, Amy Wood, Fiona Apple\nMixed and mastered by John Would",
+      "author": "Miki Meek",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Doe Meets Doe (podcast only)",
+      "brief": "Jane Doe sent some questions for us to ask Chanel Miller. For years, Chanel was known as Emily Doe. She wrote a victim impact statement that millions of people read. (A swimmer at Stanford University named Brock Turner sexually assaulted her while she was unconscious.) She talks about how she decided to come out with her real name and who Emily Doe is to her now. (9 minutes)Chanel Miller is the author of the memior Know My Name.Editor for this episode: Chana Joffe-WaltIf you’ve experienced sexual assault and want to talk to someone, you can reach out to the National Sexual Assault Hotline: 1-800-656-HOPE. If you’re having thoughts of suicide, call the National Suicide Prevention Lifeline by dialing or texting 988 on your phone.",
+      "author": "Ira GlassValerie Kipnis",
+      "timestamp": 0,
+      "song": []
+    }
+  ]
+}
