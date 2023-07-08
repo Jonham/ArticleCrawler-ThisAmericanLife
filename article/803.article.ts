d852cdc10a546227ee7c36a38c69bed259f12a3f
Jonham.Chen <jonham.chen@yichang.io>
2023-07-08 08:14:46 +0800

    add 803, 804

---
+++ article/803.article.ts
@@ -0,0 +1,60 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 803,
+  "title": "Greetings, People Of Earth",
+  "brief": "Humans encounter non-human intelligences of various kinds and try to make sense of them.",
+  "preUpdateTime": "",
+  "isUpdate": false,
+  "updateTime": "June 23, 2023",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-803-greetingspeopleofearth-gpt4.jpg?itok=PAguVkPc",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/7ff5b8fa-e5a0-4fed-bd80-197b62a2748b/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=7ff5b8fa-e5a0-4fed-bd80-197b62a2748b",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira has some thoughts about our country’s long history of alien invasion movies. (2 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "First Contact",
+      "brief": "In this past year we’ve witnessed a revolution in A.I. since the public rollout of ChatGPT.  Our Senior Editor David Kestenbaum thinks that even though there’s been a ton of coverage, there’s one thing people haven’t talked much about: have these machines gotten to the point that they’re starting to have something like human intelligence? Where they actually understand language and concepts, and can reason? He talks with scientists at Microsoft who’ve been trying to figure that out. (30 minutes)",
+      "author": "David Kestenbaum",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Meat Cute",
+      "brief": "A short piece of fiction from the perspective of aliens who’ve been scouting Earth, from writer Terry Bisson. It’s called “They're Made Out of Meat.” It’s performed by actors Maeve Higgins and H Jon Benjamin. (5 minutes)More information about Terry Bisson on his website. His latest novel is Any Day Now. Maeve Higgins’ comedy album is A Very Special Woman. H Jon Benjamin plays the title characters in the TV shows Bob’s Burgers and Archer. He is also the Jazz Daredevil on Sub Pop Records.",
+      "author": "Terry Bisson",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Yacht Rocked",
+      "brief": "A species of massive, mysterious, highly intelligent beings have recently been making contact with humanity. Or our boats, anyway. Many people seem convinced they are seeking revenge for past injustices. Producer Chris Benderev wondered if that was true. (7 minutes)April Boyes documents her sailing on her blog, Beyond The Boyes.",
+      "author": "Chris Benderev",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "Jorts and All",
+      "brief": "Many of us, especially when we’re young, feel like we’re the alien, trying to understand and fit in with the humans on this planet. Producer Diane Wu spent some time recently with a teenage humanoid who feels that way. (15 minutes)",
+      "author": "Diane Wu",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Here Come The Martian Martians” by Jonathan Richman & The Modern Lovers",
+          "name": "“Here Come The Martian Martians”",
+          "singer": "Jonathan Richman & The Modern Lovers"
+        }
+      ]
+    }
+  ]
+}
