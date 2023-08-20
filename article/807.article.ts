e5714e30140dd7fecfc52f37445fe86aa7779392
Jonham.Chen <jonham.chen@yichang.io>
2023-08-20 18:46:31 +0800

    add 807, 714

---
+++ article/807.article.ts
@@ -0,0 +1,92 @@
+import { ArticleProto } from '../parseContent'
+
+export const info: ArticleProto ={
+  "number": 807,
+  "title": "Eight Fights",
+  "brief": "Nadia's family is split between Russia and Ukraine, which is pretty common. And when Russia invaded Ukraine, it didn’t just start fighting on the battlefield. It sparked family conflict, too. An intimate story of the war from writer Masha Gessen.",
+  "preUpdateTime": "August 4, 2023",
+  "isUpdate": true,
+  "updateTime": "Aug. 4, 2023",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-807-eightfights-valerodoval.jpg?itok=8WJVmUkf",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/63135d5e-d491-4bf1-8e12-732bc99c64da/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=63135d5e-d491-4bf1-8e12-732bc99c64da",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "An extended family, and eight fights. (1 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Fight #1",
+      "title": "Where Does Luka Go?",
+      "brief": "Luka’s parents – Nadia and Karen – try to figure out where to take him once war breaks out. (6 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Fight #2",
+      "title": "Who Is Russian? And Who Is Ukrainian?",
+      "brief": "Nadia and Karen have been arguing over Russian-ness since they needed to pick a school for Luka. (10 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Fights #3 and #4",
+      "title": "Crimea and the Globe",
+      "brief": "Nadia remembers the times that Luka’s father would suggest going to Crimea for vacation, as if it wasn’t Ukrainian land occupied by Russia. And she remembers a present that Karen once gave Luka––the sort which had to be smuggled into the country. (6 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Fight #5",
+      "title": "Bucha",
+      "brief": "Nadia tells the story of her father, Alex, who lives near Bucha, and how differently he and she view the Russian atrocities there. (10 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Fight #6",
+      "title": "About Giving Money to the Russian Government",
+      "brief": "Nadia tells the story of her mother, who lives in Russia, and how she won’t do the one thing Nadia keeps asking her to do. (2 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Fight #7",
+      "title": "The Nice Showdown",
+      "brief": "Karen sends Nadia a photo which drives them to a final showdown. (12 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Fight #8",
+      "title": "The Fight That Wasn’t",
+      "brief": "Nadia’s step-father works for the Russian government. How to manage that? (4 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Epilogue",
+      "title": "Epilogue",
+      "brief": "Nadia and Karen’s son, Luka, who most of these fights are about, gets the last word. (3 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Familia Bonefireball” by Gogol Bordello",
+          "name": "“Familia Bonefireball”",
+          "singer": "Gogol Bordello"
+        }
+      ]
+    }
+  ]
+}
