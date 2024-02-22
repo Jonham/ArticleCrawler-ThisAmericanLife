907d033bf35d98a78a7e1240718a2c41b8887035
Jonham Chen <me@jonham.cn>
2024-02-22 22:50:31 +0800

    update

--- main.server.ts
+++ main.server.ts
@@ -1,12 +1,13 @@
 import * as Hapi from "@hapi/hapi";
 import { createWriteStream, mkdirSync, writeFileSync } from "fs";
-import { resolve } from "path";
+import { basename, dirname, extname, join, resolve } from "path";
 import { ArticleProto } from "./parseContent";
 import axios from "axios";
-import path = require("path");
 import * as Notify from "node-notifier";
 import { transform } from "./transform";
-import ProgressBar from 'progress'
+import ProgressBar from "progress";
+import { exec } from "child_process";
+import { writeFile } from "fs/promises";
 
 const init = async () => {
   const server = Hapi.server({
@@ -14,19 +15,19 @@ const init = async () => {
     host: "0.0.0.0",
   });
 
-  await server.register(require('@hapi/inert'));
+  await server.register(require("@hapi/inert"));
   // Doc: https://hapi.dev/tutorials/servingfiles/?lang=en_US
   server.route({
-    method: 'GET',
-    path: '/crop-cover/{param*}',
+    method: "GET",
+    path: "/crop-cover/{param*}",
     handler: {
       directory: {
-        path: path.resolve(__dirname, "static/crop-cover"),
-        index: ['index.html'],
+        path: resolve(__dirname, "static/crop-cover"),
+        index: ["index.html"],
         listing: true,
-      }
-    }
-  })
+      },
+    },
+  });
 
   server.route({
     method: "POST",
@@ -75,16 +76,36 @@ export const info: ArticleProto =${JSON.stringify(data, null, "  ")}`;
   mkdirSync(mediaFolder, { recursive: true });
 
   if (data.coverURL) {
-    console.log('> start fetching cover')
-    await fetchMedia(data.coverURL, number, mediaFolder, `fetch cover: ${data.number}`);
+    console.log("> start fetching cover");
+    const targetFilePath = await fetchMedia(
+      data.coverURL,
+      number,
+      mediaFolder,
+      `fetch cover: ${data.number}`
+    );
+    const sqrImg = join(
+      dirname(targetFilePath),
+      basename(targetFilePath) + "sqr" + extname(targetFilePath)
+    );
+    await cropImage(targetFilePath, sqrImg);
   }
   if (data.audioURL) {
-    console.log('> start fetching audio:', data.audioURL)
-    await fetchMedia(data.audioURL, number, mediaFolder, `fetch audio: ${data.number}`);
+    console.log("> start fetching audio:", data.audioURL);
+    await fetchMedia(
+      data.audioURL,
+      number,
+      mediaFolder,
+      `fetch audio: ${data.number}`
+    );
   }
 }
 
-async function fetchMedia(link: string, number: number, mediaFolder: string, hint: string = '') {
+async function fetchMedia(
+  link: string,
+  number: number,
+  mediaFolder: string,
+  hint: string = ""
+) {
   const m = link.match(/\.(png|jpg|jpeg|gif|webp|mp3|mp4)/);
   let suffix = m ? m[0] : ".png";
 
@@ -92,16 +113,35 @@ async function fetchMedia(link: string, number: number, mediaFolder: string, hin
   const res = await axios.get(link, {
     responseType,
     onDownloadProgress: (ev) => {
-      console.log('onDownloadProgress', ev)
-    }
+      console.log("onDownloadProgress", ev);
+    },
   });
 
   console.log(`audio-${number}${suffix}`, res.data.length);
   const mediaType = suffix === ".mp3" ? "audio" : "img";
-  writeFileSync(`${mediaFolder}/${mediaType}-${number}${suffix}`, res.data);
+  const targetFileName = `${mediaFolder}/${mediaType}-${number}${suffix}`;
+  await writeFile(targetFileName, res.data).catch((er) => {
+    console.log(er);
+  });
+  return targetFileName;
 }
 
-async function fetchAudio(link: string, number: number, mediaFolder: string, hint: string = '') {
+async function cropImage(p: string, target: string) {
+  const cmd = `ffmpeg -y -i ${p} -vf "pad=aspect=1:x=-1:y=-1:color=black" ${target}`;
+  return new Promise((resolve, reject) => {
+    return exec(cmd, (err) => {
+      if (err) return reject(err);
+      return resolve("ok");
+    });
+  });
+}
+
+async function fetchAudio(
+  link: string,
+  number: number,
+  mediaFolder: string,
+  hint: string = ""
+) {
   const m = link.match(/\.(webp|mp3|mp4)/);
   let suffix = m ? m[0] : ".png";
 
@@ -109,24 +149,24 @@ async function fetchAudio(link: string, number: number, mediaFolder: string, hin
   const { data, headers } = await axios.get(link, {
     responseType,
   });
-  const totalLength = headers['content-length']
+  const totalLength = headers["content-length"];
 
-  console.log('Starting download')
-  const progressBar = new ProgressBar('-> downloading [:bar] :percent :etas', {
+  console.log("Starting download");
+  const progressBar = new ProgressBar("-> downloading [:bar] :percent :etas", {
     width: 40,
-    complete: '=',
-    incomplete: ' ',
+    complete: "=",
+    incomplete: " ",
     renderThrottle: 1,
-    total: parseInt(totalLength)
-  })
+    total: parseInt(totalLength),
+  });
 
   const mediaType = suffix === ".mp3" ? "audio" : "img";
   const writer = createWriteStream(
     `${mediaFolder}/${mediaType}-${number}${suffix}`
-  )
+  );
 
-  data.on('data', (chunk) => progressBar.tick(chunk.length))
-  data.pipe(writer)
+  data.on("data", (chunk) => progressBar.tick(chunk.length));
+  data.pipe(writer);
   // console.log(`audio-${number}${suffix}`, res.data.length);
   // const mediaType = suffix === ".mp3" ? "audio" : "img";
   // writeFileSync(`${mediaFolder}/${mediaType}-${number}${suffix}`, res.data);
