ee4d6286aa6b7af2dcbe9ce461e13649ecc9be79
Jonham Chen <me@jonham.cn>
2024-07-28 22:52:07 +0800

    add records

---
+++ article/836.article.ts
@@ -0,0 +1,51 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 836,
+  "title": "The Big Rethink",
+  "brief": "People rethinking some of the most important relationships in their lives — with their sister, their political party, and the nominee for president.",
+  "preUpdateTime": "",
+  "isUpdate": false,
+  "updateTime": "July 19, 2024",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/uncropped/public/episodes/images/gettyimages-1246879172-c.jpg?itok=edvbG3kq",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/836ac87c-46f0-4cf9-a719-390d81aa96df/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=836ac87c-46f0-4cf9-a719-390d81aa96df",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira observes that we are in a moment of national reconsideration. (2 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Fate of the Union",
+      "brief": "Zoe Chace reports on a surprising guest at the Republican National Convention: Teamsters president Sean O’Brien. (18 minutes)",
+      "author": "Zoe Chace",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Out on a Limb",
+      "brief": "Ira talks to Representative Seth Moulton about what it was like to be among the first members of Congress to call for President Joe Biden to step aside. (18 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Anything You Can Do I Can Do Backwards",
+      "brief": "Two adult sisters revisit old rivalries when they compete for a world record in typing with their pinkies. (16 minutes)",
+      "author": "Elna BakerLilly SullivanNadia Reiman",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Reconsider” by Lewis Taylor",
+          "name": "“Reconsider”",
+          "singer": "Lewis Taylor"
+        }
+      ]
+    }
+  ]
+}
