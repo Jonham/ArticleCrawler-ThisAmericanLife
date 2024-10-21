eb32acb93aac1dd4c3b68787b0f999e60fa3f468
Jonham Chen <me@jonham.cn>
2024-10-21 08:04:10 +0800

    add 2 articles

---
+++ article/843.article.ts
@@ -0,0 +1,43 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 843,
+  "title": "A Little Bit of Power",
+  "brief": "So much of the fight for the presidency is coming down to battles for small slices of voters who can help throw swing states to one candidate or another. Abbas Alawieh, a leader in the Uncommitted movement, grapples with how to get his voters the thing they want.",
+  "preUpdateTime": "October 11, 2024",
+  "isUpdate": true,
+  "updateTime": "Oct. 11, 2024",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/uncropped/public/episodes/images/tal-843-littlebitofpower-getty2167189175.jpg?itok=CQBpqmN-",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/traffic.megaphone.fm/NPR6819470872.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "When you have some power, but not a lot, how do you wield it when you’re suddenly cast into the spotlight? (4 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "The Convention",
+      "brief": "Zoe Chace and Ben Terris follow Abbas Alawieh as he fights to broker a deal at the DNC – a way to potentially satisfy the people who voted “Uncommitted” in the primaries as a protest vote against Biden’s handling of the war in Israel and Gaza. (33 minutes)",
+      "author": "Ben TerrisZoe Chace",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Back Home in Michigan",
+      "brief": "Three weeks after the Democratic National Convention, Abbas speaks at a tense community meeting in Michigan about the Uncommitted organizers’ general election recommendation and hears back from voters on how they feel about the Democratic nominee at this point. (15 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Keep Pushing” by Ryan Scott & “Power Moves” by Silver Skylarks (radio only)",
+          "name": "“Keep Pushing”",
+          "singer": "Ryan Scott & “Power Moves”"
+        }
+      ]
+    }
+  ]
+}
---
+++ article/844.article.ts
@@ -0,0 +1,43 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 844,
+  "title": "This Is the Case of Henry Dee",
+  "brief": "Thirteen parole board members decide whether or not one man should be released from prison.",
+  "preUpdateTime": "October 18, 2024",
+  "isUpdate": true,
+  "updateTime": "Oct. 18, 2024",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/large_portrait/public/episodes/images/tal-844-paroleshow-andreruddock.jpg?itok=ayq8mLfn",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/traffic.megaphone.fm/NPR4575933858.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Henry Dee has been locked up for most of his life, nearly 50 years. Now, he’s up for parole. Reporter Ben Austen tells the story. (19 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Part 1",
+      "title": "Part 1",
+      "brief": "The parole board members puzzle through the pros and cons of releasing Henry Dee from prison and cast their votes. (26 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Part 2",
+      "title": "Part 2",
+      "brief": "Reporter Ben Austen continues the story. (8 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Parole Song” by Slaughter Beach, Dog",
+          "name": "“Parole Song”",
+          "singer": "Slaughter Beach, Dog"
+        }
+      ]
+    }
+  ]
+}
