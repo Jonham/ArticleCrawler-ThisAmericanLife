62252e5e5a6b94a2d97310677d0ec28ba8d18245
Jonham Chen <me@jonham.cn>
2024-11-04 08:22:24 +0800

    add 660, 845

---
+++ article/845.article.ts
@@ -0,0 +1,51 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 845,
+  "title": "A Small Thing That Gives Me a Tiny Shred of Hope ",
+  "brief": "A wee flame, flickering in the dark.",
+  "preUpdateTime": "November 1, 2024",
+  "isUpdate": true,
+  "updateTime": "Nov. 1, 2024",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/istock-1336641748.jpg?itok=bsWsYmJ7",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/traffic.megaphone.fm/NPR4945843907.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Who’s trying to bridge the gap between Blue America and Red America? Ira gets a glimpse of one guy who might be able to do just that. (3 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "A Tiny Thing That Gives Me Hope",
+      "brief": "A politically divided couple searches for a news source they both can trust. (26 minutes)The original version of this story first appeared on Question Everything, a co-production of KCRW and Placement Theory.",
+      "author": "Brian ReedZach St. LouisJonathan GoldsteinRobyn Semien",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Till Death Do Us Partisan",
+      "brief": "\"June\" is making a tactical decision about her vote this election. (13 minutes)",
+      "author": "Aviva DeKornfeldTobin Low",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Let Me Be Frank",
+      "brief": "Frank Filocomo thinks people care too much about politics when it comes to dating. His dates don’t necessarily agree. (10 minutes)We first learned about Frank from an article in The Guardian.",
+      "author": "Aviva DeKornfeldTobin Low",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Vote!” by The Linda Lindas",
+          "name": "“Vote!”",
+          "singer": "The Linda Lindas"
+        }
+      ]
+    }
+  ]
+}
