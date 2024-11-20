f28c8d38104cbcbf24b77893a57265a6c1324e15
Jonham Chen <me@jonham.cn>
2024-11-20 08:34:25 +0800

    add articles

---
+++ article/846.article.ts
@@ -0,0 +1,59 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 846,
+  "title": "This Is the Cake We Baked",
+  "brief": "With Donald Trump’s victory this week, many people looked at the election results and thought, yeah, this is the country I thought it was. For some people, that was a hopeful thing. For others, kind of the opposite. This week, we talked with people who helped make it happen and some who are looking to what’s next.",
+  "preUpdateTime": "November 8, 2024",
+  "isUpdate": true,
+  "updateTime": "Nov. 8, 2024",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/uncropped/public/episodes/images/tal-846-ap24167018953675.jpg?itok=anJ4-_fr",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/traffic.megaphone.fm/NPR3621530343.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira talks with Zoe Chace about watching Trump’s victory from an ecstatic room in Michigan. Then he checks in with a DC cop who was injured at the Capitol on January 6. (7 minutes)",
+      "author": "Ira GlassZoe Chace",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "The Largest Deportation Operation in American History",
+      "brief": "Trump has claimed that he will be able to deport between 15 and 20 million people. But neither he nor his team have spelled out exactly how they’d do it. Producer Nadia Reiman looked into what mass deportation could actually look like on the ground if and when it comes to pass. (17 minutes)",
+      "author": "Nadia Reiman",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Nobody Loves Our Latino Community and our Puerto Rican Community More Than I Do",
+      "brief": "Trump won record numbers of Latino voters this year. Ike Sriskandarajah spent the day with a guy in Pennsylvania who's been working to bring Latino voters to Trump for years. (15 minutes)",
+      "author": "Ike Sriskandarajah",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Come Retribution",
+      "brief": "Ira talks with two of Trump’s “political enemies” about their post-election plans. (8 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "I’ve Done an Unbelievable Job on the Abortion Question",
+      "brief": "Ten different states had abortion rights measures on their ballots this election. Producer Miki Meek got curious about a particular kind of political ad that aired in many of those states and called up a few of the women whose stories were featured in them. (9 minutes)We first learned about Deborah Dorbert and Kimberly Paseka from articles in The Washington Post.",
+      "author": "Miki MeekDana ChivvisHenry Larson",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Morning in America” by Durand Jones & The Indications",
+          "name": "“Morning in America”",
+          "singer": "Durand Jones & The Indications"
+        }
+      ]
+    }
+  ]
+}
