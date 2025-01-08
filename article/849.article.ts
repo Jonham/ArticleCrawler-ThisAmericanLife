a0599973a78a74b43c56a86626622e0a190f4494
Jonham Chen <me@jonham.cn>
2025-01-08 16:39:00 +0800

    add several cases

---
+++ article/849.article.ts
@@ -0,0 +1,43 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 849,
+  "title": "The Narrator",
+  "brief": "Banias is an 8-year-old kid living in Gaza. And she has a story to tell — many stories, in fact.",
+  "preUpdateTime": "December 13, 2024",
+  "isUpdate": true,
+  "updateTime": "Dec. 13, 2024",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/uncropped/public/episodes/images/tal-849-narrator-3.jpg?itok=XrEo9evG",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chrt.fm/track/138C95/pdst.fm/e/traffic.megaphone.fm/NPR9970452314.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "While on the phone with reporter Maram Hamaid in Gaza, producer Chana Joffe-Walt gets interrupted by Maram’s daughter––Banias, eight, who grabs the phone from her mother and starts telling us about her life. The narrator arrives. (8 minutes)",
+      "author": "Chana Joffe-Walt",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Part One",
+      "title": "Part One",
+      "brief": "Banias, an 8-year-old in Gaza, tells us about her life––her friends, the games she plays, the things she cares about. Everything but the war going on around her. (25 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Part Two",
+      "title": "Part Two",
+      "brief": "Banias talks about the war. (20 minutes)",
+      "author": "",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Barbie Girl” by Aqua",
+          "name": "“Barbie Girl”",
+          "singer": "Aqua"
+        }
+      ]
+    }
+  ]
+}
---
+++ article/850.article.ts
@@ -0,0 +1,63 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 850,
+  "title": "If You Want to Destroy My Sweater, Hold This Thread as I Walk Away",
+  "brief": "The tiny thing that unravels your world.",
+  "preUpdateTime": "December 20, 2024",
+  "isUpdate": true,
+  "updateTime": "Dec. 20, 2024",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-805-mysweater-16x9_0.jpg?itok=sX5eLTWD",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chrt.fm/track/138C95/pdst.fm/e/traffic.megaphone.fm/NPR8545196799.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira talks to Chris Benderev, whose high school years were completely upended by an impromptu thing his teacher said. (8 minutes)",
+      "author": "Ira GlassChris Benderev",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "The World Has Turned and Left Me Here",
+      "brief": "For Producer Lilly Sullivan, there’s one story about her parents that defines how she sees them, their family, and their history. She finds out it might be wrong. (27 minutes)",
+      "author": "Lilly Sullivan",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“You Ain’t Going Nowhere” by  The Byrds (podcast only)",
+          "name": "“You Ain’t Going Nowhere”",
+          "singer": "The Byrds (podcast only)"
+        }
+      ]
+    },
+    {
+      "index": "Act Two",
+      "title": "What’s With These Homies Dissing My Girl?",
+      "brief": "For years, Mike Comite has replayed in his head the moment when he and his bandmate blew their shot of making it as musicians. He sets out to uncover how it all went awry. (13 minutes)Julia Nunes’s music is available on Spotify.",
+      "author": "Mike ComiteDiane Wu",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Losers” by Belle Brigade. Covered by Julia Nunes and Mike Comite",
+          "name": "“Losers”",
+          "singer": "Belle Brigade. Covered"
+        }
+      ]
+    },
+    {
+      "index": "Act Three",
+      "title": "And If You See Her, Tell Her It’s Over Now",
+      "brief": "Six million Syrians fled the country after the start of its civil war. A few weeks ago, one woman watched from afar as everything in her home country changed forever – again. (9 minutes)",
+      "author": "Diane WuHany Hawasly",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Undone” by Weezer. Covered by Mike Comite and Julia Nunes",
+          "name": "“Undone”",
+          "singer": "Weezer. Covered"
+        }
+      ]
+    }
+  ]
+}
