9a81d5d5ee2694ce178f2c805c3a675d6f7bee94
Jonham Chen <me@jonham.cn>
2025-01-30 09:40:18 +0800

    add 3 articles

--- article/823.article.ts
+++ article/823.article.ts
@@ -1,14 +1,13 @@
 import { ArticleProto } from '../parseContent'
-
 export const info: ArticleProto ={
   "number": 823,
   "title": "The Question Trap",
   "brief": "An investigation of when and why people ask loaded questions that are a proxy for something else.",
   "preUpdateTime": "February 2, 2024",
   "isUpdate": true,
-  "updateTime": "Feb. 2, 2024",
+  "updateTime": "Jan. 24, 2025",
   "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/istock-1429988030.jpg?itok=0sDAR4EA",
-  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chtbl.com/track/8DB4DB/pdst.fm/e/nyt.simplecastaudio.com/bbbcc290-ed3b-44a2-8e5d-5513e38cfe20/episodes/5e22d88d-a289-4135-801a-57e6941a1ea3/audio/128/default.mp3?awCollectionId=bbbcc290-ed3b-44a2-8e5d-5513e38cfe20&awEpisodeId=5e22d88d-a289-4135-801a-57e6941a1ea3",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chrt.fm/track/138C95/pdst.fm/e/traffic.megaphone.fm/NPR4251190593.mp3",
   "acts": [
     {
       "index": "Prologue",
@@ -21,16 +20,22 @@ export const info: ArticleProto ={
     {
       "index": "Act One",
       "title": "Question: Tell Me What You Think About This",
-      "brief": "“What do you think about Beyoncé?” and other questions that are asked a lot, raised by people on first dates. (12 minutes)",
+      "brief": "“What do you think about Beyoncé?” and other questions raised by people on first dates. (12 minutes)",
       "author": "Tobin Low",
       "timestamp": 0,
-      "song": []
+      "song": [
+        {
+          "raw": "“Partition” by Beyoncé (radio only)",
+          "name": "“Partition”",
+          "singer": "Beyoncé (radio only)"
+        }
+      ]
     },
     {
       "index": "Act Two",
       "title": "How Old Are Your Kids?",
       "brief": "When a common, seemingly innocuous question goes wildly off the rails. (13 minutes)Here's the video this story is about, featuring comedian Adam Ray.",
-      "author": "Chris Benderev",
+      "author": "Chris BenderevBethel Habte",
       "timestamp": 0,
       "song": []
     },
@@ -50,9 +55,9 @@ export const info: ArticleProto ={
       "timestamp": 0,
       "song": [
         {
-          "raw": "“Don't Ask Me” by Joe Jackson",
+          "raw": "“Don't Ask Me” by Joe Jackson & “Don’t Ask Me” by OK Go (radio only)",
           "name": "“Don't Ask Me”",
-          "singer": "Joe Jackson"
+          "singer": "Joe Jackson & “Don’t Ask Me”"
         }
       ]
     }
---
+++ article/851.article.ts
@@ -0,0 +1,59 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 851,
+  "title": "Try a Little Tenderness",
+  "brief": "In the new year, stories of people trying a radical approach to solving their problems.",
+  "preUpdateTime": "January 10, 2025",
+  "isUpdate": true,
+  "updateTime": "Jan. 10, 2025",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/uncropped/public/episodes/images/tal-851-drawing-16x9.jpg?itok=k-U18uSa",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chrt.fm/track/138C95/pdst.fm/e/traffic.megaphone.fm/NPR2941405405.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira meets two sisters who got into a fight, and then learned a lesson in turning the other cheek. (8 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "The Big Nap",
+      "brief": "A hardened PI works the toughest case of his very young life. (18 minutes)ALL IN: COMEDY ABOUT LOVE is written by Simon Rich and stars John Mulaney, Richard Kind, Fred Armisen, and Chloe Fineman. It runs through February 16 at the Hudson Theater in New York.",
+      "author": "Simon Rich",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "The Gladiator Starring Ruffled Crow",
+      "brief": "Producer Aviva DeKornfeld talks to a man who finds himself the target of vengeful crows. (8 minutes)",
+      "author": "Aviva DeKornfeld",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "You’ll Spank Me Later For This",
+      "brief": "Comedian Josh Johnson wonders if some people should’ve been spanked as kids. (10 minutes)",
+      "author": "Josh Johnson",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "The Feels on the Bus",
+      "brief": "Writer Etgar Keret reads his story about a bus driver who refuses to open the doors for late passengers. (9 minutes)This story is from Etgar's book, The Bus Driver Who Wanted To Be God & Other Stories. Etgar sends stories and poems to subscribers in his newsletter, Alphabet Soup.",
+      "author": "Etgar Keret",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Try A Little Tenderness” by Otis Redding",
+          "name": "“Try A Little Tenderness”",
+          "singer": "Otis Redding"
+        }
+      ]
+    }
+  ]
+}
