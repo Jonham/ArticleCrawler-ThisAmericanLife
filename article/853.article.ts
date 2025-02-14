3dd87e25d6a76fba0fb6c923dc87f3dd8b27e7d5
Jonham Chen <me@jonham.cn>
2025-02-14 23:47:02 +0800

    update

---
+++ article/853.article.ts
@@ -0,0 +1,73 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 853,
+  "title": "Groundhog Day",
+  "brief": "People stuck in a loop, trying to find their way out.",
+  "preUpdateTime": "January 31, 2025",
+  "isUpdate": true,
+  "updateTime": "Jan. 31, 2025",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-853-groundhogday-2.jpg?itok=iS3CsXlG",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chrt.fm/track/138C95/pdst.fm/e/traffic.megaphone.fm/NPR4398997362.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Host Ira Glass talks to B.A. Parker about her birthday tradition. (6 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Will You Still Slug Me Tomorrow?",
+      "brief": "Producer Aviva DeKornfeld speaks with a father and daughter who have been playing the same game for 25 years. (9 minutes)",
+      "author": "Aviva DeKornfeld",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "I’ll Repeat the Question",
+      "brief": "Talia Augustidis asks a single question over and over. (5 minutes)A version of this story originally aired on Short Cuts, a Falling Tree production for BBC Radio 4.",
+      "author": "Talia Augustidis",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Raiders of the Lost Chard",
+      "brief": "Editor David Kestenbaum speaks with Jeff Permar, who is trapped in a Groundhog Day situation — with an actual groundhog! (9 minutes)See more videos of the groundhog Chunk on Jeff’s YouTube channel.",
+      "author": "David Kestenbaum",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Groundhogs in my Garden” by Scott Bullock (radio only)",
+          "name": "“Groundhogs in my Garden”",
+          "singer": "Scott Bullock (radio only)"
+        }
+      ]
+    },
+    {
+      "index": "Act Four",
+      "title": "Heart of Parkness",
+      "brief": "Parking in a big city can be a real pain. Producer Valerie Kipnis speaks with a man who has taken it upon himself to try to mitigate the weekly hassle. (14 minutes)",
+      "author": "Valerie Kipnis",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Five",
+      "title": "It's Been a Hard Year's Night, and I've Been Working Like a Hog",
+      "brief": "Short fiction from Bess Kalb about a groundhog named Susan, who has her own opinions about the holiday named after her species. (7 minutes)You can find more work from Bess at The Grudge Report on Substack.",
+      "author": "Bess KalbBim AdewunmiAviva DeKornfeld",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Stuck on the Treadmill” by Richard Thompson & “Punxsutawney Rock” by Tari Kelly & Tim Minchin (radio only)",
+          "name": "“Stuck on the Treadmill”",
+          "singer": "Richard Thompson & “Punxsutawney Rock”"
+        }
+      ]
+    }
+  ]
+}
