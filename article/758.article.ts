8805c04c2e797499180d76d5737724302ef04240
Jonham Chen <me@jonham.cn>
2025-02-23 16:33:34 +0800

    add 2

---
+++ article/758.article.ts
@@ -0,0 +1,43 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 758,
+  "title": "Talking While Black",
+  "brief": "President Trump is eradicating DEI from the federal government, and private companies are following his example. We return to a show we did two years ago about the turning point that led to this moment. Our Executive Producer Emanuele Berry guest-hosts and shares stories about Black people who found themselves caught in the middle of this cultural fight when the country shifted decisively away from diversity, equity, inclusion, critical race theory, and affirmative action.",
+  "preUpdateTime": "January 7, 2022",
+  "isUpdate": true,
+  "updateTime": "Feb. 14, 2025",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal_758_trevor_davis.png?itok=2IUEIJFm",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chrt.fm/track/138C95/pdst.fm/e/traffic.megaphone.fm/NPR3891692679.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "As a new high school principal, Dr. Whitfield felt moved by the national renouncement of racism he saw all around him in the wake of George Floyd’s murder. It prompted him to write a thoughtful email to parents and teachers in his district. He got lots of praise for it. Less than a year later, that same email would threaten his job. (12 minutes)",
+      "author": "Emanuele Berry",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Incident",
+      "brief": "During her sophomore year in high school, Nevaeh was targeted in a secret text message chain by a handful of her peers. She’d come to learn the text chat was a mock slave trade where her photo and photos of other Black classmates were uploaded, talked about as property, and bid on. Emanuele Berry talks to Nevaeh about what these messages mean to her now as well as how she’s navigated her town’s reaction and her close friendships with kids who mostly aren’t Black. (20 minutes)",
+      "author": "Emanuele Berry",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "The Farce Awakens",
+      "brief": "After the murder of George Floyd, sales of books by Black authors skyrocketed. Now, there are efforts to ban many of the same books. Producer Chana Joffe-Walt talks to author Jerry Craft, who is caught up in this backlash with his graphic novel New Kid. (21 minutes)",
+      "author": "Chana Joffe-Walt",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Set My Sails” by Flame",
+          "name": "“Set My Sails”",
+          "singer": "Flame"
+        }
+      ]
+    }
+  ]
+}
---
+++ article/854.article.ts
@@ -0,0 +1,37 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 854,
+  "title": "Ten Things I Don't Want To Hate About You",
+  "brief": "Zach Mack and his dad try to mend a rift between them … in a very unusual way.",
+  "preUpdateTime": "February 21, 2025",
+  "isUpdate": true,
+  "updateTime": "Feb. 21, 2025",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal_854_owen_freeman.png?itok=RKiPoHTa",
+  "audioURL": "https://www.thisamericanlife.org/sites/default/files/audio/upload/promos/854.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Ira Glass introduces Zach Mack’s story. (1 minute)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Part One",
+      "title": "Part One",
+      "brief": "Zach and his father enter into an agreement that could change their entire relationship. (9 minutes)",
+      "author": "Zach MackDavid Kestenbaum",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Part Two",
+      "title": "Part Two",
+      "brief": "Zach’s mother and sister weigh in on the agreement. (28 minutes)",
+      "author": "Zach MackDavid Kestenbaum",
+      "timestamp": 0,
+      "song": []
+    }
+  ]
+}
