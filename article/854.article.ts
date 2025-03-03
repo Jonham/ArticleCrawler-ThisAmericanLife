6c4177a370e3380636634bca32a931d4ce2e5821
Jonham Chen <me@jonham.dev>
2025-03-03 20:39:03 +0800

    add 854, 855

--- article/854.article.ts
+++ article/854.article.ts
@@ -1,13 +1,13 @@
 import { ArticleProto } from '../parseContent'
 export const info: ArticleProto ={
   "number": 854,
-  "title": "Ten Things I Don't Want To Hate About You",
-  "brief": "Zach Mack and his dad try to mend a rift between them … in a very unusual way.",
+  "title": "Ten Things I Don't Want to Hate About You",
+  "brief": "Zach Mack and his dad try to mend a rift between them in a very unusual way.",
   "preUpdateTime": "February 21, 2025",
   "isUpdate": true,
   "updateTime": "Feb. 21, 2025",
   "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal_854_owen_freeman.png?itok=RKiPoHTa",
-  "audioURL": "https://www.thisamericanlife.org/sites/default/files/audio/upload/promos/854.mp3",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chrt.fm/track/138C95/pdst.fm/e/traffic.megaphone.fm/NPR4214593193.mp3",
   "acts": [
     {
       "index": "Prologue",
@@ -29,9 +29,23 @@ export const info: ArticleProto ={
       "index": "Part Two",
       "title": "Part Two",
       "brief": "Zach’s mother and sister weigh in on the agreement. (28 minutes)",
-      "author": "Zach MackDavid Kestenbaum",
+      "author": "",
       "timestamp": 0,
       "song": []
+    },
+    {
+      "index": "Part Three",
+      "title": "Part Three",
+      "brief": "With the year coming to an end, someone is going to have to say, “You were right, and I was wrong.” Will it change anything? (16 minutes)This story is a collaboration with NPR's Embedded podcast, which released a three-part series about Zach and his father.",
+      "author": "",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Suspicious Minds” by Elvis Presley",
+          "name": "“Suspicious Minds”",
+          "singer": "Elvis Presley"
+        }
+      ]
     }
   ]
 }
---
+++ article/855.article.ts
@@ -0,0 +1,51 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 855,
+  "title": "That’s a Weird Thing to Lie About",
+  "brief": "Unnecessary and outrageous lies that make you wonder — why lie about that in the first place?",
+  "preUpdateTime": "February 28, 2025",
+  "isUpdate": true,
+  "updateTime": "Feb. 28, 2025",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/tal-855-aweirdthingtolieabout-yifanwu.png?itok=07L8CV3J",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chrt.fm/track/138C95/pdst.fm/e/traffic.megaphone.fm/NPR4492330255.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Kasey, a woman who prides herself on her truthfulness, tries to help host Ira Glass figure out how to stop lying about one specific thing. (10 minutes)",
+      "author": "Ira GlassDiane Wu",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "The Real L Word",
+      "brief": "Producer Dana Chivvis talks to Liz Flock about a strange experience she had in 2011. (21 minutes)Liz Flock’s most recent book is The Furies.",
+      "author": "Dana Chivvis",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Bully Pulpit",
+      "brief": "Host Ira Glass talks with M. Gessen about a lie they've been seeing out in the world a lot recently — the “bully lie.” (15 minutes)M. Gessen’s book on Donald Trump and his autocratic tendencies is Surviving Autocracy.",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "In Defense of Unnecessary Lies",
+      "brief": "We find someone brave enough to stand up and make a case FOR lying. That person is producer Ike Sriskandarajah. (8 minutes)",
+      "author": "Ike Sriskandarajah",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Little Lies” by Fleetwood Mac",
+          "name": "“Little Lies”",
+          "singer": "Fleetwood Mac"
+        }
+      ]
+    }
+  ]
+}
