8b2d96d548d02ac64af9ba2a359435c4e5d4ed23
Jonham Chen <me@jonham.dev>
2025-03-12 19:36:40 +0800

    add 535

---
+++ article/535.article.ts
@@ -0,0 +1,59 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 535,
+  "title": "Origin Story",
+  "brief": "Little-known and surprising stories of how all sorts of institutions began.",
+  "preUpdateTime": "September 19, 2014",
+  "isUpdate": true,
+  "updateTime": "Mar. 7, 2025",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/istock-1404256098.jpg?itok=v7xG4YVD",
+  "audioURL": "https://pfx.vpixl.com/6qj4J/dts.podtrac.com/redirect.mp3/chrt.fm/track/138C95/pdst.fm/e/traffic.megaphone.fm/NPR2755711351.mp3",
+  "acts": [
+    {
+      "index": "Prologue",
+      "title": "Prologue",
+      "brief": "Host Ira Glass talks to business professor Pino Audia and Fast Company magazine columnist Dan Heath about corporate creation myths and why so many of them involve garages. (7 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act One",
+      "title": "Mad Man",
+      "brief": "Sarah Koenig tells the story of her father, Julian Koenig, the legendary advertising copywriter whose work includes the slogan \"Timex takes a licking and keeps on ticking\" and Volkswagen's \"Think Small\" ads. For years, Sarah has heard her dad accuse a former partner of stealing some of his best ideas, but until recently, she never paid much attention. Then she started asking her dad for details of this fight for his legacy, and what she learned surprised her. (20 minutes)Sarah Koenig is the host and co-creator of Serial.",
+      "author": "Sarah Koenig",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Two",
+      "title": "Silent Partner",
+      "brief": "Producer Sean Cole visits Chad's Trading Post in Southampton, Massachusetts. One person who works there wears a shirt that says \"Chad's Brother;\" other shirts say \"Chad's Best Friend,\" \"Chad's Cousin,\" and \"Chad's Father.\" Pictures of Chad are everywhere. Chad's dead. The family explains. (14 minutes)",
+      "author": "Sean Cole",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Three",
+      "title": "Wait Wait… Don't Film Me",
+      "brief": "Peter Sagal, host of NPR's Wait Wait...Don't Tell Me, tells Ira the origin story of one of the worst movie sequels ever made. (5 minutes)",
+      "author": "Peter Sagal",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Act Four",
+      "title": "Bill Clinton's 7-Year-Old Brother",
+      "brief": "Reporter Mary Wiltenburg tells the story of a little boy stymied by the question \"Where do you come from?\" (8 minutes)You can read more of the family’s stories on Mary’s website.",
+      "author": "Mary Wiltenburg",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Dear John” by John Lennon",
+          "name": "“Dear John”",
+          "singer": "John Lennon"
+        }
+      ]
+    }
+  ]
+}
