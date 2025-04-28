c66bd77fe1aec36a6e56afbbbf3f886a90d78b24
Jonham Chen <me@jonham.dev>
2025-04-28 22:49:16 +0800

    add articles

---
+++ article/332.article.ts
@@ -0,0 +1,111 @@
+import { ArticleProto } from '../parseContent'
+export const info: ArticleProto ={
+  "number": 332,
+  "title": "The Ten Commandments",
+  "brief": "For Easter weekend — and the end of Passover! — stories of people struggling to follow the Ten Commandments.",
+  "preUpdateTime": "May 4, 2007",
+  "isUpdate": true,
+  "updateTime": "Apr. 18, 2025",
+  "coverURL": "https://www.thisamericanlife.org/sites/default/files/styles/landscape/public/episodes/images/332-bricktestament.jpeg?itok=nfi3RAZe",
+  "audioURL": "https://www.thisamericanlife.org/sites/default/files/audio/upload/promos/332.mp3",
+  "acts": [
+    {
+      "index": "",
+      "title": "Prologue",
+      "brief": "Host Ira Glass reads from the Ten Commandments. Not the original Ten Commandments, but some of the newer, lesser-known ones. There's the Miner's Ten Commandments of 1853, the Ten Commandments of Umpiring, and the Ten Commandments for Math Teachers — just to name a few. (4 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“The Ten Commandments of Love” by Peaches & Herb",
+          "name": "“The Ten Commandments of Love”",
+          "singer": "Peaches & Herb"
+        }
+      ]
+    },
+    {
+      "index": "Commandments One, Two and Three",
+      "title": "Honor God",
+      "brief": "As a boy in religious school, Shalom Auslander is informed that his name, Shalom, is one of the names of God, and so he must be very careful not to take his own name in vain. (9 minutes)Shalom Auslander is the author of Beware of God: Stories and Foreskin's Lament.",
+      "author": "Shalom Auslander",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Commandment Four",
+      "title": "Keep The Sabbath Holy",
+      "brief": "Six houses of worship in six different cities, each with its own way of honoring the Sabbath. (3 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Commandment Five",
+      "title": "Honor Your Father And Your Mother",
+      "brief": "When Jack Hitt was 11, he did the worst thing his father could have imagined. Neither Jack nor his four siblings will ever forget the punishment. (6 minutes)",
+      "author": "Jack Hitt",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Commandment Six",
+      "title": "You Shall Not Murder",
+      "brief": "Alex Blumberg talks to Lt. Col. Lyn Brown, an Army Reserve chaplain who served two tours in Iraq. Brown talks about what \"thou shalt not kill\" means to soldiers on the battlefield. (6 minutes)",
+      "author": "Alex Blumberg",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“Kish” by David Byrne",
+          "name": "“Kish”",
+          "singer": "David Byrne"
+        }
+      ]
+    },
+    {
+      "index": "",
+      "title": "",
+      "brief": "",
+      "author": "",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Commandment Seven",
+      "title": "You Shall Not Commit Adultery",
+      "brief": "In the book of Matthew, Jesus says that looking lustfully at a woman is like committing adultery in your heart. Contributor David Dickerson was raised as an evangelical Christian, and for many years tried not to have a single lustful thought. (9 minutes)",
+      "author": "David Ellis Dickerson",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Commandment Eight",
+      "title": "You Shall Not Steal",
+      "brief": "Ira talks to a waiter named Hassan at Liebman's Deli in the Bronx about some audacious thefts he's witnessed in his years in the restaurant business. (3 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Commandment Nine",
+      "title": "You Shall Not Bear False Witness",
+      "brief": "Chaya Lipschutz wanted to donate one of her kidneys to a stranger. But to save a stranger's life, she had to break the commandment against lying. And the person she had to lie to was her mother. Chaya talked to Sarah Koenig. (8 minutes)",
+      "author": "Sarah Koenig",
+      "timestamp": 0,
+      "song": []
+    },
+    {
+      "index": "Commandment Ten",
+      "title": "You Shall Not Covet",
+      "brief": "Ira talks to seventh-graders about the things they covet most. (4 minutes)",
+      "author": "Ira Glass",
+      "timestamp": 0,
+      "song": [
+        {
+          "raw": "“The Ten Commandments of Love” by Peaches & Herb",
+          "name": "“The Ten Commandments of Love”",
+          "singer": "Peaches & Herb"
+        }
+      ]
+    }
+  ]
+}
